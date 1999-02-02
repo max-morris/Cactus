8fb97b72aca6fe3bac33ab295ad8a65e0f43be25
Tom Goodale <goodale@cct.lsu.edu>
1999-02-02 18:28:32 +0000

    New files to contain dummy stuff until we work out what to do with it.
    Tom
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@185 17b73243-c579-4c4c-a9d2-2d5706c11dac

---
+++ src/include/Dummies.h
@@ -0,0 +1,43 @@
+ /*@@
+   @header    Dummies.h
+   @date      Tue Feb  2 19:01:45 1999
+   @author    Tom Goodale
+   @desc 
+   A file to contain prototypes for some dummy functions until the real ones 
+   are written. 
+   @enddesc 
+ @@*/
+
+#ifndef _DUMMIES_H_
+#define _DUMMIES_H_
+
+#ifdef _cplusplus
+extern "C" {
+#endif
+
+int CCTK_DummyStorageOn(void *GH, int group);
+
+int CCTK_DummyStorageOff(void *GH, int group);
+
+int CCTK_DummyCommunicationOn(void *GH, int group);
+
+int CCTK_DummyCommunicationOff(void *GH, int group);
+
+int CCTK_DummyTriggerable(int variable);
+
+int CCTK_DummyTriggerSaysGo(int variable);
+
+int CCTK_DummyTriggerAction(void *GH, int group);
+
+int CCTK_DummyCallFunc(void *GH, int language, void *function);
+
+
+#ifdef _cplusplus
+}
+#endif
+
+
+#endif
+
+
+
---
+++ src/main/Dummies.c
@@ -0,0 +1,61 @@
+ /*@@
+   @file      Dummies.c
+   @date      Tue Feb  2 18:56:38 1999
+   @author    Tom Goodale
+   @desc 
+   A file to contain some dummy functions until the real ones are written. 
+   @enddesc 
+ @@*/
+
+#include "Dummies.h"
+
+static char *rcsid = "$Id$";
+
+int CCTK_DummyStorageOn(void *GH, int group)
+{
+  return 0;
+}
+
+int CCTK_DummyStorageOff(void *GH, int group)
+{
+  return 0;
+}
+
+int CCTK_DummyCommunicationOn(void *GH, int group)
+{
+  return 0;
+}
+
+int CCTK_DummyCommunicationOff(void *GH, int group)
+{
+  return 0;
+}
+
+int CCTK_DummyTriggerable(int variable)
+{
+  return 0;
+}
+
+int CCTK_DummyTriggerSaysGo(int variable)
+{
+  return 0;
+}
+
+int CCTK_DummyTriggerAction(void *GH, int group)
+{
+  return 0;
+}
+
+int CCTK_DummyCallFunc(void *GH, int language, void *function)
+{
+
+  void (*calledfunc)(void *);
+
+  calledfunc = (void (*)(void *))function;
+
+  /* Call the function. */
+  
+  calledfunc(GH);
+
+  return 0;
+}
