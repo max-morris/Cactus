24149a45367577aa02c87a36d8cc2ac178c11b74
Tom Goodale <goodale@cct.lsu.edu>
1999-02-02 13:20:04 +0000

    Written RegisterGHExtension.
    Tom
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@172 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/comm/GHExtensions.c
+++ src/comm/GHExtensions.c
@@ -7,9 +7,21 @@
    @enddesc 
  @@*/
 
+#include <stdlib.h>
+
 #include "flesh.h"
+#include "StoreHandledData.h"
 
 static char *rcsid = "$Id$";
+
+static cHandledData *GHExtensions = NULL;
+static int num_extensions = 0;
+
+struct GHExtension
+{
+  int number;
+  void *(*init)(cGH *);
+};
 
 int CCTK_TraverseGHExtensions(cGH *GH, const char *when)
 {
@@ -18,10 +30,60 @@ int CCTK_TraverseGHExtensions(cGH *GH, const char *when)
 
 int CCTK_RegisterGHExtension(cGH *GH, const char *name)
 {
-  return -1;
+  int return_val;
+
+  int handle;
+
+  struct GHExtension *new_extension;
+
+  /* Check that the extension hasn't already been registered */
+  handle = CCTK_GetHandle(GHExtensions, name, NULL);
+
+  if(handle < 0)
+  {
+    /* New extension. */
+    new_extension = (struct GHExtension *)malloc(sizeof(struct GHExtension));
+
+    if(new_extension)
+    {
+      handle = CCTK_NewHandle(&GHExtensions, name, new_extension);
+
+      new_extension->init = NULL;
+      
+      /* Remember how many extensions there are */
+      num_extensions++;
+    }
+    else
+    {
+      /* Memory failure. */
+      handle = -2;
+    }
+  }
+  else
+  {
+    /* Extension already exists. */
+    handle = -1;
+  }
+    
+  return handle;
 }
 
-int CCTK_RegisterGHExtensionInitialiser(int handle, void (*func)())
+int CCTK_RegisterGHExtensionInitialiser(int handle, void *(*func)(cGH *))
 {
-  return -1;
+  int return_code;
+  struct GHExtension *extension;
+
+  extension = CCTK_GetHandledData(GHExtensions, handle);
+
+  if(extension)
+  {
+    extension->init = func;
+    return_code = 1;
+  }
+  else
+  {
+    return_code = 0;
+  }
+
+  return return_code;
 }
