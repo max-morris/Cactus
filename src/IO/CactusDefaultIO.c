b4840d9cf1988dfd27778f52ec13539304f81521
Gabrielle Allen <gdallen@illinois.edu>
1999-02-08 11:32:56 +0000

    The default IO routines which for now just print a statement
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@214 17b73243-c579-4c4c-a9d2-2d5706c11dac

---
+++ src/IO/CactusDefaultIO.c
@@ -0,0 +1,31 @@
+ /*@@
+   @file      CactusDefaultIO.c
+   @date      Mon Jan 8 1999
+   @author    Gabrielle Allen
+   @desc 
+   Default cactus IO routines.
+   @enddesc 
+ @@*/
+
+
+#include "flesh.h"
+#include "CactusIODefaults.h"
+
+static char *rcsid = "$Header:";
+
+int CactusDefaultOutputGH(cGH *GH)
+{
+  printf("I'm in the default OutputGH routine\n");
+  
+  return 0;
+}
+
+int CactusDefaultOutputVarAs(cGH *GH, 
+                             const char *var,   
+                             const char *method, 
+                             const char *alias)
+{
+  printf("I'm in the default OutputVarAs routine\n");
+  
+  return 0;
+}
