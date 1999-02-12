9d28d7b98558f7107c02d50e61086a6a0bf755c7
Tom Goodale <goodale@cct.lsu.edu>
1999-02-12 16:22:19 +0000

    Header file for the cactus rfr interface stuff.
    Tom
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@242 17b73243-c579-4c4c-a9d2-2d5706c11dac

---
+++ src/include/CactusrfrInterface.h
@@ -0,0 +1,42 @@
+ /*@@
+   @header    CactusrfrInterface.h
+   @date      Thu Feb 11 18:42:03 1999
+   @author    Tom Goodale
+   @desc 
+   A file to contain prototypes for the interface between Cactus and the rfr.
+   @enddesc 
+ @@*/
+
+#ifndef _CACTUSRFRINTERFACE_H_
+#define _CACTUSRFRINTERFACE_H_
+
+#ifdef _cplusplus
+extern "C" {
+#endif
+
+int CCTK_rfrStorageOn(void *GH, int group);
+
+int CCTK_rfrStorageOff(void *GH, int group);
+
+int CCTK_rfrCommunicationOn(void *GH, int group);
+
+int CCTK_rfrCommunicationOff(void *GH, int group);
+
+int CCTK_rfrTriggerable(int variable);
+
+int CCTK_rfrTriggerSaysGo(int variable);
+
+int CCTK_rfrTriggerAction(void *GH, int group);
+
+int CCTK_rfrCallFunc(void *GH, int language, void *function);
+
+
+#ifdef _cplusplus
+}
+#endif
+
+
+#endif
+
+
+
