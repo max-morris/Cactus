96ce0ae422cea6e3edd3bf9083fa7e180eaaf156
Gabrielle Allen <gdallen@illinois.edu>
1999-02-13 19:58:39 +0000

    Added more variables (global_sh, nghostzones, convlevel) to fortran argument list
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@252 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctk.h
+++ src/include/cctk.h
@@ -19,10 +19,13 @@
 /* Define some stuff */
 
 #ifdef FCODE
-#define _CCTK_FARGUMENTS  dim,sh, lb, ub, bbox, levfac, GH
+#define _CCTK_FARGUMENTS  dim, global_sh, sh, lb, ub, bbox, levfac, convlevel, nghostzones, GH
 #define _DECLARE_CCTK_FARGUMENTS INTEGER dim&&\
+                           INTEGER global_sh(dim)&&\
                            INTEGER sh(dim), lb(dim), ub(dim), bbox(2*dim)&&\
                            INTEGER levfac&&\
+                           INTEGER convlevel&&\
+                           INTEGER nghostzones&&\
                            POINTER GH&&\
 
 #endif /*FCODE*/
@@ -31,11 +34,17 @@
 #define _INITIALISE_CCTK_C2F
 #define _DECLARE_CCTK_C2F 
 #define _PASS_CCTK_C2F(xGH) &((xGH)->dim),\
+                            (xGH)->global_shape,\
                             (xGH)->local_shape, (xGH)->lower_bound, (xGH)->upper_bound, (xGH)->bbox,\
                             &((xGH)->levfac),\
+                            &((xGH)->convlevel),\
+                            &((xGH)->nghostzones),\
                             (xGH)
 #define _CCTK_C2F_PROTO     int *,\
+                            int *,\
                             int *,int *, int *, int *,\
+                            int *,\
+                            int *,\
                             int *,\
                             cGH *
 
