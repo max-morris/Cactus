48333cf262fc0a9eddf2129de90db6420020a63c
Gabrielle Allen <gdallen@illinois.edu>
1999-02-17 12:45:57 +0000

    Header file for passing strings from fortran to C
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@299 17b73243-c579-4c4c-a9d2-2d5706c11dac

---
+++ src/include/FortranString.h
@@ -0,0 +1,48 @@
+ /*@@
+   @header    FortranString.h
+   @date      Wed Feb  17 12:55 1999
+   @author    Gabrielle Allen, Paul Walker
+   @desc 
+   Macros for dealing with strings passed from fortran to C.
+   The memory for the new arguments created should be freed after use   
+   @enddesc 
+   @version $Header$
+ @@*/
+
+
+#if defined T3E
+#include <fortran.h>
+#define ONE_FORTSTRING_ARG\
+    _fcd fcdarg
+#define TWO_FORTSTRINGS_ARGS\
+    _fcd fcd_n, _fcd fcd_w
+#define ONE_FORTSTRING_CREATE(argn)\
+       int len = _fcdlen(fcdarg); \
+       char *argn    = CCTK_NullTerminateString(_fcdtocp(fcdarg),len);;
+#define TWO_FORTSTRINGS_CREATE(argn,argw)\
+       int  nl    = _fcdlen(fcd_n);\
+       int  wl    = _fcdlen(fcd_w);\
+       char *argn = CCTK_NullTerminateString(_fcdtocp(fcd_n),nl);\
+       char *argw = CCTK_NullTerminateString(_fcdtocp(fcd_w),wl);
+#elif defined WIN32
+#define ONE_FORTSTRING_ARG\
+   char *n, unsigned int nl
+#define TWO_FORSTRINGS_ARGS\
+   char *n, int nl, char *w, int wl
+#define ONE_FORTSTRING_CREATE(argn)\
+        char *argn = CCTK_NullTerminateString(n,nl);
+#define TWO_FORTSTRINGS_CREATE(argn,argw)\
+    char *argn = CCTK_NullTerminateString(n,nl);\
+    char *argw = CCTK_NullTerminateString(w,wl);
+#else
+#define ONE_FORTSTRING_ARG\
+   char *n, unsigned int nl
+#define TWO_FORTSTRINGS_ARGS\
+   const char *n, const char *w, int nl, int wl
+#define ONE_FORTSTRING_CREATE(argn)\
+        char *argn = CCTK_NullTerminateString(n,nl);
+#define TWO_FORTSTRINGS_CREATE(argn,argw)\
+    char *argn = CCTK_NullTerminateString(n,nl);\
+    char *argw = CCTK_NullTerminateString(w,wl);
+#endif
+
