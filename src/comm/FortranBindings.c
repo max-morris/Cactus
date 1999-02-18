3ae5ed7b0534da60b0d79e5eab8e03fd61b3d773
Gabrielle Allen <gdallen@illinois.edu>
1999-02-18 15:06:14 +0000

    Started added fortran bindings for the comm functions callable from fortran
    Gab
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@308 17b73243-c579-4c4c-a9d2-2d5706c11dac

---
+++ src/comm/FortranBindings.c
@@ -0,0 +1,29 @@
+ /*@@
+   @file      FortranBindings.c
+   @date      Thu Feb  18 14:27:18 1999
+   @author    Gabrielle Allen
+   @desc 
+   Fortran bindings for the comm functions
+   @enddesc 
+ @@*/
+
+#include "flesh.h"
+#include "Comm.h"
+
+int FORTRAN_NAME(CCTK_Exit)(cGH *GH)
+{
+  CCTK_Exit(GH);
+  return 0;
+}
+
+int FORTRAN_NAME(CCTK_ParallelInit)(cGH *GH)
+{
+  CCTK_ParallelInit(GH);
+  return 0;
+}
+
+int FORTRAN_NAME(CCTK_Abort)(cGH *GH)
+{
+  CCTK_Abort(GH);
+  return 0;
+}
