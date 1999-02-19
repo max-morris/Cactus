65333fcf2e556b5dd1aff1dbb80d2b75565d6189
Gabrielle Allen <gdallen@illinois.edu>
1999-02-19 10:34:04 +0000

    Added fortran binding for CCTK_SyncGroup
    Gab
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@318 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/comm/FortranBindings.c
+++ src/comm/FortranBindings.c
@@ -8,6 +8,7 @@
  @@*/
 
 #include "flesh.h"
+#include "FortranString.h"
 #include "Comm.h"
 
 int FORTRAN_NAME(CCTK_Exit)(cGH *GH)
@@ -25,5 +26,13 @@ int FORTRAN_NAME(CCTK_ParallelInit)(cGH *GH)
 int FORTRAN_NAME(CCTK_Abort)(cGH *GH)
 {
   CCTK_Abort(GH);
+  return 0;
+}
+
+int FORTRAN_NAME(CCTK_SyncGroup)(cGH *GH, ONE_FORTSTRING_ARG)
+{
+  ONE_FORTSTRING_CREATE(group_name)
+  CCTK_SyncGroup(GH,group_name);
+  free(group_name); 
   return 0;
 }
