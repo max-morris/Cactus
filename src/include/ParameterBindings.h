d8530b4b53bf14d70aa07397e0e143c2fdff841d
Tom Goodale <goodale@cct.lsu.edu>
1999-02-25 16:54:03 +0000

    Forgot to commit this yesterday.  Contains the definitions for the return codes from GetParameters.
    Tom
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@344 17b73243-c579-4c4c-a9d2-2d5706c11dac

---
+++ src/include/ParameterBindings.h
@@ -0,0 +1,20 @@
+ /*@@
+   @file      ParameterBindings.h
+   @date      Mon Feb 22 15:01:14 1999
+   @author    Tom Goodale
+   @desc 
+   Defines for parameter stuff
+   @enddesc 
+ @@*/
+
+#ifndef _PARAMTERBINDINGS_H_
+#define _PARAMTERBINDINGS_H_
+
+#define PARAMETER_KEYWORD  1
+#define PARAMETER_STRING   2
+#define PARAMETER_SENTENCE 3
+#define PARAMETER_INTEGER  4
+#define PARAMETER_REAL     4
+#define PARAMETER_LOGICAL  5
+
+#endif
