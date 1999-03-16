bb676fd202e74a34e1508156d06a4ff53f813d5f
Gabrielle Allen <gdallen@illinois.edu>
1999-03-16 10:22:35 +0000

    Added TriggerOutput
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@400 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/IOMethods.h
+++ src/include/IOMethods.h
@@ -2,5 +2,6 @@ struct IOMethod
 {
   int    (*OutputGH)(cGH *);
   int    (*OutputVarAs)(cGH *, const char *, const char *);
+  int    (*TriggerOutput)(cGH *, int);
   int    (*TimeToOutput)(cGH *,int);
 };
