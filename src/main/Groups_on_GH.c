64a895cc8776d5b75aabe6d4f7a06ed5d20f2454
Gabrielle Allen <gdallen@illinois.edu>
1999-04-06 23:59:28 +0000

    Routines for groups which need the GH
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@445 17b73243-c579-4c4c-a9d2-2d5706c11dac

---
+++ src/main/Groups_on_GH.c
@@ -0,0 +1,130 @@
+ /*@@
+   @file      Groups_on_GH.c
+   @date      Tues April 6
+   @author    Gabrielle Allen
+   @desc 
+   GH specific Routines to deal with groups.
+   @enddesc 
+ @@*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include "flesh.h"
+#include "Misc.h"
+#include "Groups.h"
+
+/*#define GROUPSDEBUG*/
+
+static char *rcsid = "$Header$";
+
+
+ /*@@
+   @routine    CCTK_GetVarDataPtr_ByName
+   @date       Tue 6th April 1999
+   @author     Gabrielle Allen
+   @desc 
+   Passes back a variable data pointer, given a full name and timelevel
+   @enddesc 
+   @calls     
+   @history 
+ 
+   @endhistory 
+
+@@*/
+void *CCTK_GetVarDataPtr_ByName(cGH *GH, char *fullvarname, int timelevel)
+{
+  return GH->data[CCTK_VarIndex(fullvarname)][timelevel];
+}
+
+ /*@@
+   @routine    CCTK_GetVarDataPtr_ByIndex
+   @date       Tue 6th April 1999
+   @author     Gabrielle Allen
+   @desc 
+   Passes back a variable data pointer, given a variable index and timelevel
+   @enddesc 
+   @calls     
+   @history 
+ 
+   @endhistory 
+
+@@*/
+void *CCTK_GetVarDataPtr_ByIndex(cGH *GH, int varindex, int timelevel)
+{
+  return GH->data[varindex][timelevel];
+}
+
+ /*@@
+   @routine    CCTK_GetVarDataPtr
+   @date       Tue 6th April 1999
+   @author     Gabrielle Allen
+   @desc 
+   Passes back a variable data pointer, given either a  variable index 
+   or a full name and timelevel
+   @enddesc 
+   @calls     
+   @history 
+ 
+   @endhistory 
+
+@@*/
+void *CCTK_GetVarDataPtr(cGH *GH, int varindex, char *fullvarname, int timelevel)
+{
+  if (fullvarname) 
+  {
+    return CCTK_GetVarDataPtr_ByName(cGH *GH, char *fullvarname, int timelevel);
+  }
+  else
+  {
+    return CCTK_GetVarDataPtr_ByIndex(cGH *GH, int varindex, int timelevel);
+  }
+}
+
+
+
+
+
+
+ /*@@
+   @routine    CCTK_ArrayGroupSize
+   @date       Mon Feb  8 12:05:50 1999
+   @author     Tom Goodale
+   @desc 
+   Gets the size of a array in a specific direction.
+   @enddesc 
+   @calls     
+   @calledby   
+   @history 
+ 
+   @endhistory 
+
+@@*/
+int *CCTK_ArrayGroupSize(cGH *GH, const char *group, int dir)
+{
+  /* Quick fudge */
+  return &(GH->local_shape[dir]);
+}
+
+ /*@@
+   @routine    CCTK_QueryGroupStorage
+   @date       
+   @author     Tom Goodale
+   @desc 
+
+   @enddesc 
+   @calls     
+   @calledby   
+   @history 
+ 
+   @endhistory 
+
+@@*/
+int CCTK_QueryGroupStorage(cGH *GH, const char *group)
+{
+  /* Quick fudge */  
+  return 1;
+}
+
