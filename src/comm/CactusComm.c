a6c82c8f696650ad6a7ed075f39d83277900e141
Gabrielle Allen <gdallen@illinois.edu>
1999-04-15 10:03:57 +0000

    Added CCTK_QueryGroupStorage
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@481 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/comm/CactusComm.c
+++ src/comm/CactusComm.c
@@ -16,3 +16,13 @@ int *CCTK_ArrayGroupSize(cGH *GH, const char *groupname, int dir)
 {
   return CCTK_ArrayGroupSize_ByBoth(GH,-1,groupname,dir);
 }
+
+int CCTK_QueryGroupStorage_ByIndex(cGH *GH, int index)
+{
+  return CCTK_QueryGroupStorage_ByBoth(GH,index , NULL);
+}
+
+int CCTK_QueryGroupStorage(cGH *GH, const char *groupname)
+{
+  return CCTK_QueryGroupStorage_ByBoth(GH, -1, groupname);
+}
