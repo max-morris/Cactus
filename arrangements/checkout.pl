13bc43630d18aa72d15e337a9fd1dc91468fa932
Gabrielle Allen <gdallen@illinois.edu>
1999-07-02 14:01:31 +0000

    Little perl script for checking out thorns or packages, still to do
    1) Add it to make file with target "gmake checkout"
    
    2) Perhaps only report on packages/thorns in the repository that
       you don't already have checked out
    
    3) Perhaps only report on conflicts when checking out, and not
       on all updates.
    
    Comments cheerfully received
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@615 17b73243-c579-4c4c-a9d2-2d5706c11dac

---
+++ arrangements/checkout.pl
@@ -0,0 +1,173 @@
+# /usr/bin/perl -s
+
+print "\n";
+
+
+    print "Checkout packages or thorns? [packages] : ";
+    
+    $which = <STDIN>;
+
+    if ($which =~ /^t/i)
+    {
+	&get_thorns();
+    }
+    else
+    { 
+	&get_packages();
+    }
+
+print "\nQuit or checkout more packages or thorns [quit] : ";
+$dowhat = <STDIN>;
+print $dowhat;
+if ($dowhat !~ /^t/i && $dowhat !~/^p/i)
+{
+    print "All done!\n";
+    exit;
+}
+
+$doit = 1;
+while ()
+{
+    
+    if ($dowhat =~ /^t/i)
+    {
+	&get_thorns();
+    }
+    else
+    { 
+	&get_packages();
+    }
+
+    print "\nQuit or checkout more packages or thorns [quit] : ";
+
+    $dowhat = <STDIN>;
+    if ($dowhat !~ /^t/i && $dowhat !~/^p/i)
+    {
+	print "All done!\n";
+	exit;
+    }    
+}
+
+sub get_packages
+{
+
+
+print "\nYou already have packages: \n";
+
+open(HAVE,"find . -type d -maxdepth 1 |");
+while (<HAVE>)
+{
+ if (!/^\.\/CVS$/ && !/^\.$/)
+ {
+   /\.\/(.*)/;
+   print "  $1\n";
+ }
+}
+
+print "\nAvailable packages: \n";
+
+open(MODULES,"cvs co -s | ");
+
+$count = 0;
+while(<MODULES>)
+{
+ if (/(\w*)\s*PACKAGE/)
+ {
+   $count++;
+   $name{$count} = $1;
+ }
+}
+
+
+for ($i=1; $i<$count+1;$i++)
+{
+  print "  [$i] $name{$i}\n";
+}
+
+print "\n";
+
+print "Checkout packages [1-$count] : ";
+
+$range = <STDIN>;
+
+while ($range =~/^([0-9]+(?:-[0-9]+)?),?/)
+{
+ $range = $';
+ $1 =~ /^([0-9]*)(-[0-9]*)?$/;
+ $first = $1;
+ if (!$2) 
+	{$last=$1}
+ else
+        {$2=~/-([0-9]*)/; $last=$1}
+
+ for ($i=$first; $i<$last+1; $i++)
+ {
+  system("cvs checkout $name{$i}");
+ }
+
+}
+}
+
+
+
+sub get_thorns
+{
+
+
+print "\nYou already have thorns: \n";
+
+open(HAVE,"find . -type d -maxdepth 2 |");
+while (<HAVE>)
+{
+ if (!/CVS$/ && /\.\/\w*\/\w*$/)
+ {
+   /\.\/(.*)/;
+   print "  $1\n";
+ }
+}
+
+print "\nAvailable thorns: \n";
+
+open(MODULES,"cvs -q co -s | ");
+
+$count = 0;
+while(<MODULES>)
+{
+ if (/(\w*\/?\w*)\s*THORN/)
+ {
+   $count++;
+   $name{$count} = $1;
+ }
+}
+
+
+for ($i=1; $i<$count+1;$i++)
+{
+  print "  [$i] $name{$i}\n";
+}
+
+print "\n";
+
+print "Checkout thorns [1-$count] : ";
+
+$range = <STDIN>;
+
+while ($range =~/^([0-9]+(?:-[0-9]+)?),?/)
+{
+ $range = $';
+ $1 =~ /^([0-9]*)(-[0-9]*)?$/;
+ $first = $1;
+ if (!$2) 
+	{$last=$1}
+ else
+        {$2=~/-([0-9]*)/; $last=$1}
+
+ for ($i=$first; $i<$last+1; $i++)
+ {
+  system("cvs -q checkout $name{$i}");
+ }
+
+}
+}
+
+
