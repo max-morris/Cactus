c6274ba9720dbfaa0cd0f005181547b0c460201c
Gabrielle Allen <gdallen@illinois.edu>
1999-07-03 16:46:55 +0000

    Changes for T3E
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@626 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/FortranString.h
+++ src/include/FortranString.h
@@ -25,12 +25,7 @@
 #define TWO_FORTSTRINGS_ARGS\
     _fcd fcd_n, _fcd fcd_w
 #define THREE_FORTSTRINGS_ARGS\
-   const char *CCTK_str1,\
-   const char *CCTK_str2,\
-   const char *CCTK_str3,\
-   unsigned int CCTK_len1,\
-   unsigned int CCTK_len2,\
-   unsigned int CCTK_len3
+    _fcd fcd_arg1, _fcd fcd_arg2, _fcs fcd_arg3
 #define ONE_FORTSTRING_CREATE(argn)\
        int len = _fcdlen(fcdarg); \
        char *argn    = CCTK_NullTerminateString(_fcdtocp(fcdarg),len);;
@@ -40,9 +35,12 @@
        char *argn = CCTK_NullTerminateString(_fcdtocp(fcd_n),nl);\
        char *argw = CCTK_NullTerminateString(_fcdtocp(fcd_w),wl);
 #define THREE_FORTSTRINGS_CREATE(arg1,arg2,arg3)\
-   char *arg1 = CCTK_NullTerminateString(CCTK_str1,CCTK_len1);\
-   char *arg2 = CCTK_NullTerminateString(CCTK_str2,CCTK_len2);\
-   char *arg3 = CCTK_NullTerminateString(CCTK_str3,CCTK_len3);
+       int  narg1    = _fcdlen(fcd_arg1);\
+       int  narg2    = _fcdlen(fcd_arg2);\
+       int  narg3    = _fcdlen(fcd_arg3);\
+       char *arg1 = CCTK_NullTerminateString(_fcdtocp(fcd_arg1),narg1);\
+       char *arg2 = CCTK_NullTerminateString(_fcdtocp(fcd_arg2),narg2);\
+       char *arg3 = CCTK_NullTerminateString(_fcdtocp(fcd_arg3),narg3);
 
 #elif defined WIN32
 
