f7010a1951b00ab2f6ba023dda35580abb55ebae
Gabrielle Allen <gdallen@illinois.edu>
1999-07-04 12:00:26 +0000

    Contains CST_error(level,message,line,file);
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@627 17b73243-c579-4c4c-a9d2-2d5706c11dac

---
+++ lib/sbin/CSTUtils.pl
@@ -0,0 +1,34 @@
+sub CST_error
+{
+    local($level,$mess,$line,$file) = @_;
+
+    if ($full_warnings)
+    {
+	if ($level == 0)
+	{
+	    $CST_errors++;
+	    print STDERR "CST error in $file (at $line)\n";
+	    print STDERR "  -> $mess\n";
+	}
+	else
+	{
+	    print STDERR "CST warning in $file (at $line)\n";
+	    print STDERR "  -> $mess\n";
+	}
+    }
+    else
+    {
+	if ($level == 0)
+	{
+	    $CST_errors++;
+	    print STDERR "CST error $CST_errors:\n  -> $mess\n";
+	}
+	else
+	{
+	    print STDERR "CST warning:\n  -> $mess\n";
+	}	    
+    }
+    return;
+}
+
+1;
