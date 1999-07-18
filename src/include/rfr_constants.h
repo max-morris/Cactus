022ad8220996351d7b86318199d9e05b31758a4f
Gabrielle Allen <gdallen@illinois.edu>
1999-07-18 10:34:36 +0000

    Removed all the numbered RFR points, now you have to use before and after. Actually added one as well. CCTK_POSTINITIAL ... this is because you sometimes have to do something after the initial data is created, this would previously have been CCTK_INITIAL9 (for example, save the initial lapse). Now CCTK_INITIAL should only be for actually setting up initial data (I think), anything else should be POSTINITIAL or BASEGRID.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@725 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/rfr_constants.h
+++ src/include/rfr_constants.h
@@ -16,38 +16,14 @@
 #define CCTK_RECOVER   5
 
 /* Be sure to keep the INITIAL*, POSTSTEP*, etc... contiguous */
-#define CCTK_INITIAL0  10
 #define CCTK_INITIAL   11
-#define CCTK_INITIAL2  12
-#define CCTK_INITIAL3  13
-#define CCTK_INITIAL4  14
-#define CCTK_INITIAL5  15
-#define CCTK_INITIAL6  16
-#define CCTK_INITIAL7  17
-#define CCTK_INITIAL8  18
-#define CCTK_INITIAL9  19
+#define CCTK_POSTINITIAL   12
 
 #define CCTK_CPINITIAL 25
 
 #define CCTK_PRESTEP   30
-#define CCTK_PRESTEP2  31
-#define CCTK_PRESTEP3  33
-#define CCTK_PRESTEP4  34
-#define CCTK_PRESTEP5  35
-
 
 #define CCTK_POSTSTEP   40
-#define CCTK_POSTSTEP2  41
-#define CCTK_POSTSTEP3  42
-#define CCTK_POSTSTEP4  43
-#define CCTK_POSTSTEP5  44
-#define CCTK_POSTSTEP6  45
-#define CCTK_POSTSTEP7  46
-#define CCTK_POSTSTEP8  47
-#define CCTK_POSTSTEP9  48
-#define CCTK_POSTSTEP10 49
-
-
 
 #define CCTK_EVOL       60
 
--- src/main/rfrInterface.c
+++ src/main/rfrInterface.c
@@ -89,49 +89,27 @@ int CCTK_rfrPrintTree(cGH *GH,void *rfr_top)
 
     showed_tree = 1;
 
+    printf ("\n-------------------------------------------------------------------------------------\n");
     printf ("\nEvolution tree from the RFR\n\n");
 
     rfrPrintDescs(rfr_top,GH,CCTK_BASEGRID,"CCTK_BASEGRID","");
-    
-    rfrPrintDescs(rfr_top,GH,CCTK_INITIAL0,"CCTK_INITIAL0","");
     rfrPrintDescs(rfr_top,GH,CCTK_INITIAL, "CCTK_INITIAL","");
-    rfrPrintDescs(rfr_top,GH,CCTK_INITIAL2,"CCTK_INITIAL2","");
-    rfrPrintDescs(rfr_top,GH,CCTK_INITIAL3,"CCTK_INITIAL3","");
-    rfrPrintDescs(rfr_top,GH,CCTK_INITIAL4,"CCTK_INITIAL4","");
-    rfrPrintDescs(rfr_top,GH,CCTK_INITIAL5,"CCTK_INITIAL5","");
-    rfrPrintDescs(rfr_top,GH,CCTK_INITIAL6,"CCTK_INITIAL6","");
-    rfrPrintDescs(rfr_top,GH,CCTK_INITIAL7,"CCTK_INITIAL7","");
-    rfrPrintDescs(rfr_top,GH,CCTK_INITIAL8,"CCTK_INITIAL8","");
-    rfrPrintDescs(rfr_top,GH,CCTK_INITIAL9,"CCTK_INITIAL9","");
+    rfrPrintDescs(rfr_top,GH,CCTK_INITIAL, "CCTK_POSTINITIAL","");
     printf ("\n  (Don't forget CCTK_POSTSTEP* is here too...)\n");
     printf ("\n  do loop over timesteps\n");
     rfrPrintDescs(rfr_top,GH,CCTK_PRESTEP,"CCTK_PRESTEP","   ");
-    rfrPrintDescs(rfr_top,GH,CCTK_PRESTEP2,"CCTK_PRESTEP2","   ");
-    rfrPrintDescs(rfr_top,GH,CCTK_PRESTEP3,"CCTK_PRESTEP3","   ");
-    rfrPrintDescs(rfr_top,GH,CCTK_PRESTEP4,"CCTK_PRESTEP4","   ");
-    rfrPrintDescs(rfr_top,GH,CCTK_PRESTEP5,"CCTK_PRESTEP5","   ");
-    
     rfrPrintDescs(rfr_top,GH,CCTK_EVOL,"CCTK_EVOL","   ");
     rfrPrintDescs(rfr_top,GH,CCTK_BOUND,"CCTK_BOUND","   ");
     
     printf ("     t = t+dt\n");
-    
     rfrPrintDescs(rfr_top,GH,CCTK_POSTSTEP,"CCTK_POSTSTEP","   ");
-    rfrPrintDescs(rfr_top,GH,CCTK_POSTSTEP2,"CCTK_POSTSTEP2","   ");
-    rfrPrintDescs(rfr_top,GH,CCTK_POSTSTEP3,"CCTK_POSTSTEP3","   ");
-    rfrPrintDescs(rfr_top,GH,CCTK_POSTSTEP4,"CCTK_POSTSTEP4","   ");
-    rfrPrintDescs(rfr_top,GH,CCTK_POSTSTEP5,"CCTK_POSTSTEP5","   ");
-    rfrPrintDescs(rfr_top,GH,CCTK_POSTSTEP6,"CCTK_POSTSTEP6","   ");
-    rfrPrintDescs(rfr_top,GH,CCTK_POSTSTEP7,"CCTK_POSTSTEP7","   ");
-    rfrPrintDescs(rfr_top,GH,CCTK_POSTSTEP8,"CCTK_POSTSTEP8","   ");
-    rfrPrintDescs(rfr_top,GH,CCTK_POSTSTEP9,"CCTK_POSTSTEP9","   ");
-    rfrPrintDescs(rfr_top,GH,CCTK_POSTSTEP10,"CCTK_POSTSTEP10","   ");
     
     printf ("     if (time-for-output)\n");
     rfrPrintDescs(rfr_top,GH,CCTK_ANALYSIS,"CCTK_ANALYSIS","      ");
     printf ("     endif\n");
     printf ("   enddo\n");
-    printf ("End of tree from RFR\n\n");
+    printf ("\n-------------------------------------------------------------------------------------\n");
+
   }
 
   return 1;
