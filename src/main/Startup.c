68fa54bb73070461d473a8a9bc07ffc1ec8276bb
Gabrielle Allen <gdallen@illinois.edu>
1999-07-18 13:16:21 +0000

    Changes to banner string for compiler
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@735 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/main/Startup.c
+++ src/main/Startup.c
@@ -4,21 +4,7 @@ int Cactus_Startup(void)
   
   const char *string;
 
-  string = "
-        \\ |/                                              
-        /--\\                                              
-      \\|   .|-                    \\  |  |  |  /  
-       | .  |                                             
-       | .  |-                   -  Cactus 4.0  -  
-      -|    |                                             
-       |  / |                     /  |  |  |  \\ 
-      \\|    |/                                               
-       | .  |           Thorny problems in Numerical Relativity and Beyond
-       |    |-                                            
-       \\    /     Credits: Tom Goodale, Joan Masso, Paul Walker and Gabrielle Allen 
-      --------        Including contributions from many worldwide collaborators 
-      \\______/  
-                ";
+  string = "        \\ |/\n        /--\\\n      \\|   .|-                    \\  |  |  |  /  \n       | .  |\n       | .  |-                   -  Cactus 4.0  -\n      -|    |\n       |  / |                     /  |  |  |  \\\n      \\|    |/\n       | .  |           Thorny problems in Numerical Relativity and Beyond\n       |    |-\n       \\    /     Credits: Tom Goodale, Joan Masso, Paul Walker and Gabrielle Allen\n      --------        Including contributions from many worldwide collaborators\n      \\______/\n                ";
 
   CCTK_RegisterBanner(string);
 
