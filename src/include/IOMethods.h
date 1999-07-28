73d57294e9fead6c500814c505172408cbf24b2f
Thomas Radke <tradke@aei.mpg.de>
1999-07-28 17:17:05 +0000

    Added extern "C" declaration
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@809 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/IOMethods.h
+++ src/include/IOMethods.h
@@ -6,9 +6,17 @@ struct IOMethod
   int    (*TimeToOutput)(cGH *,int);
 };
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int CCTK_RegisterIOMethod(const char *name);
 int CCTK_RegisterIOMethodOutputGH(int handle, int (*func)(cGH *));
 int CCTK_RegisterIOMethodTimeToOutput(int handle, int (*func)(cGH *, int));
 int CCTK_RegisterIOMethodTriggerOutput(int handle, int (*func)(cGH *, int));
 int CCTK_RegisterIOMethodOutputVarAs(int handle, int (*func)(cGH *,
                                      const char *,const char *));
+#ifdef __cplusplus
+}
+#endif
+
