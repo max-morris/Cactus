4e912ef960482adaecaaf1216e2c7fb6e86114b0
Gabrielle Allen <gdallen@illinois.edu>
1999-09-14 11:30:26 +0000

    Routine to copy example parameter files to one directory
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@915 17b73243-c579-4c4c-a9d2-2d5706c11dac

---
+++ lib/sbin/CopyParFiles.pl
@@ -0,0 +1,43 @@
+#!/bin/perl -s
+#
+# Script to copy parameter files
+# Version: $Id$
+
+$home = `pwd`;
+chop($home);
+
+$config = $ARGV[0];
+
+open(THORNLIST,"<configs/$config/ThornList") || die "ThornList not available";
+
+if (! -d "thornparfiles")
+{
+    mkdir("thornparfiles", 0755) || die "Unable to create thornparfiles directory";
+}
+
+while (<THORNLIST>)
+{
+ $thorn = $_;
+ chop($thorn);
+ print "Thorn $thorn\n";
+ $thorn = "arrangements/$thorn/par";
+ if (-d $thorn)
+ {
+   chdir $thorn;
+   while ($parfile = <*.par>)
+   { 
+     if (-e "$home/thornparfiles/$parfile")
+     {
+        print "  $parfile exists, will not overwrite\n";
+     }
+     else
+     {
+       print "  Copying $parfile\n";
+       system("cp $parfile $home/thornparfiles/$parfile");
+     }
+   }
+   
+   chdir "$home${sep}";
+
+ }
+}
