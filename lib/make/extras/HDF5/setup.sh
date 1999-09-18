f2b541eed2def8a0abc12f8c6bae296c53327457
Thomas Radke <tradke@aei.mpg.de>
1999-09-18 19:16:19 +0000

    Configure now automatically figures the HDF5 version (serial or parallel).
    MPI can be used with both versions (for the serial one Cactus won't make
    use of the parallel HDF5 extensions).
    If Cactus is not configured with MPI also, only the serial HDF5 version is
    accepted.
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@957 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- lib/make/extras/HDF5/setup.sh
+++ lib/make/extras/HDF5/setup.sh
@@ -8,7 +8,9 @@
 #   @enddesc 
 # @@*/
     
-if test -n "$HDF5" ; then
+if test -z "$HDF5" ; then
+  exit 0
+fi
 	
 echo "Configuring with HDF5. Blocks with #ifdef HDF5 will be activated"
 
@@ -16,12 +18,41 @@ CCTK_WriteLine cctk_extradefs.h "#define HDF5"
 
 # Work out which variation of HDF5 lib
     
-if test -r $srcdir/extras/HDF5/$HDF5 ; then
-	. $srcdir/extras/HDF5/$HDF5
-else
-    echo "HDF5 selected, but no known HDF5 method - what is $HDF5 ?"
-    exit 2
+if test -z "$HDF5_DIR" ; then
+   echo "HDF5 selected but no HDF5_DIR set... Checking some places"
+   CCTK_Search HDF5_DIR "/usr /usr/local /usr/local/hdf5 /usr/local/packages/hdf5 /usr/local/apps/hdf5 c:/packages/hdf5" include/hdf5.h
+   if test -z "$HDF5_DIR" ; then
+       echo "Unable to locate the HDF5 directory - please set HDF5_DIR"
+       exit 2
+   fi
+   echo "Found an HDF5 package in $HDF5_DIR"
 fi
+
+grep -qe '#define HAVE_PARALLEL 1' ${HDF5_DIR}/include/H5config.h 2> /dev/null
+test_phdf5=$?
+
+if [ -n "$MPI" ] ; then
+   if [ $test_phdf5 -eq 0 ] ; then
+      echo "Found parallel HDF5 library, so Cactus will make use of PHDF5 support."
+   else
+      echo "Found serial HDF5 library, so Cactus can't make use of PHDF5 support."
+   fi
+else
+   if [ $test_phdf5 -eq 0 ] ; then
+      echo "Found parallel HDF5 library, but Cactus wasn't configured with MPI."
+      echo "Please set HDF5_DIR to point to a serial HDF5 package, or configure Cactus with MPI."
+      exit 2
+   fi
+fi
+
+# Set the HDF5 libs, libdirs and includedirs
+
+HDF5_LIBS=hdf5
+if test `uname` = "Linux" ; then
+  HDF5_LIBS="hdf5 z"
+fi
+HDF5_LIB_DIRS="$HDF5_DIR/lib"
+HDF5_INC_DIRS="$HDF5_DIR/include"
 
 # Write the data out to the header and make files.
 
@@ -35,6 +66,3 @@ CCTK_WriteLine make.extra.defn ""
 CCTK_WriteLine make.extra.defn 'LIBS         += $(HDF5_LIBS)'
 CCTK_WriteLine make.extra.defn 'LIBDIRS      += $(HDF5_LIB_DIRS)'
 CCTK_WriteLine make.extra.defn 'SYS_INC_DIRS += $(HDF5_INC_DIRS)'
-
-fi
-
