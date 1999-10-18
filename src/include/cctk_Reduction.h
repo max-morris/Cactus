b8b9a8c5b6925cbed6ba46650accdc7398651b90
Gerd Lanfermann <lanfer@aei.mpg.de>
1999-10-18 08:22:10 +0000

    forgot a ;
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1047 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctk_Reduction.h
+++ src/include/cctk_Reduction.h
@@ -63,7 +63,7 @@ int CCTK_ReduceLocalScalar (cGH *GH, int proc, int operation_handle,
 
 int CCTK_ReduceLocalArray1D (cGH *GH, int proc, int operation_handle,
                             void *in_array1d, void *out_array1d, 
-			    int num_in_array1d, int data_type)
+			    int num_in_array1d, int data_type);
 
 
 int CCTK_ReduceArray(cGH *GH,
