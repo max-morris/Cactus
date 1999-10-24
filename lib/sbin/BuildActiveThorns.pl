396f793c3c436e4148c09b03299847654182f6eb
Gabrielle Allen <gdallen@illinois.edu>
1999-10-24 10:17:30 +0000

    Print a comment at the top of the ThornList
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1088 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- lib/sbin/BuildActiveThorns.pl
+++ lib/sbin/BuildActiveThorns.pl
@@ -15,6 +15,8 @@ $package_dir = shift(@ARGV);
 
 %info = &buildthorns($package_dir,"thorns");
 
+printf("# arrangement/thorn %-14s # implements (inherits) [friend] {shares}\n#\n");
+
 foreach $thorn (sort keys %info)
 {
   printf("%-34s # %s\n",$thorn,$info{$thorn});
