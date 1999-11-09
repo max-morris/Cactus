7cfc39be9d0a3e747b74dab2da4f33a87dee5059
Gerd Lanfermann <lanfer@aei.mpg.de>
1999-11-09 12:17:15 +0000

    CCTK_FortranString has to return the lenght of the string as CCTK_INT, not int because this gives pointer errors (on the T3E)
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1147 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/util/Misc.c
+++ src/util/Misc.c
@@ -972,7 +972,7 @@ int CCTK_RegexMatch(const char *string,
 @@*/
  
  
-void FORTRAN_NAME(CCTK_FortranString)(int *nchar, char **cstring,ONE_FORTSTRING_ARG)
+void FORTRAN_NAME(CCTK_FortranString)(CCTK_INT *nchar, char **cstring,ONE_FORTSTRING_ARG)
 {
 
   int i;
