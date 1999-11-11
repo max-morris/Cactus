9cc400bea70f205819ca2453df931fee131ccff5
Gabrielle Allen <gdallen@illinois.edu>
1999-11-11 09:52:12 +0000

    Andres change so the repository checking works on the T3E
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1150 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- lib/sbin/checkout.pl
+++ lib/sbin/checkout.pl
@@ -495,9 +495,10 @@ sub RepositoryExists
   my($repository) = @_;
   my $dummy;
 
-  open(MODULES,"cvs -d $_[0] co -s |");
-  $dummy = <MODULES>;
-  close(MODULES);
+#  open(MODULES,"cvs -d $_[0] co -s |");
+  $dummy = join('', qx {cvs -d $_[0] co -s});
+#  $dummy = <MODULES>;
+#  close(MODULES);
 
   return !$?;
 
