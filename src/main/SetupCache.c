45096745ace3316779a7b93125d3798b742ef7a9
Tom Goodale <goodale@cct.lsu.edu>
1999-11-30 23:50:31 +0000

    New cache alignment stuff.
    Tom
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1178 17b73243-c579-4c4c-a9d2-2d5706c11dac

---
+++ src/main/SetupCache.c
@@ -0,0 +1,66 @@
+ /*@@
+   @file      SetupCache.c
+   @date      Tue Nov 30 10:30:09 1999
+   @author    Tom Goodale
+   @desc 
+   Sets up cache stuff for the CCTK
+   @enddesc 
+ @@*/
+
+static char *rcsid = "$Header$";
+
+#include "cctk.h"
+#include "cctki_Cache.h"
+#include "cctk_parameters.h"
+
+/********************************************************************
+ *********************   CCTK Local Routines   **********************
+ ********************************************************************/
+
+ /*@@
+   @routine    CCTKi_SetupCache
+   @date       Tue Nov 30 10:50:02 1999
+   @author     Tom Goodale
+   @desc 
+   Sets the cache information.
+   @enddesc 
+   @calls     
+   @calledby   
+   @history 
+ 
+   @endhistory 
+
+   @returntype int
+   @returndesc
+   0
+   @endreturndesc
+
+@@*/
+int CCTKi_SetupCache(void)
+{
+  DECLARE_CCTK_PARAMETERS
+
+  unsigned long cache_size;
+  unsigned long cacheline_bytes;
+
+  if(manual_cache_setup)
+  {
+    cache_size = manual_cache_size;
+    cacheline_bytes = manual_cacheline_bytes;
+  }
+  else
+  {
+    /* FIXME:  Remove this check for release */
+#ifdef CCTK_L2_CACHE_SIZE
+    cache_size      = CCTK_L2_CACHE_SIZE;
+    cacheline_bytes = CCTK_L2_CACHELINE_BYTES;
+#else
+    cache_size      = 0;
+    cacheline_bytes = 0;
+#endif
+  }
+
+  CCTKi_CacheDataSet(cacheline_bytes, cache_size);
+
+  return 0;
+}
---
+++ src/util/Cache.c
@@ -0,0 +1,226 @@
+ /*@@
+   @file      Cache.c
+   @date      Tue Nov 30 08:07:12 1999
+   @author    Tom Goodale
+   @desc 
+   Routines dealing with cache alignment.
+   @enddesc 
+ @@*/
+
+static char *rcsid = "$Header$";
+
+#include <stdlib.h>
+
+#include "cctk_Cache.h"
+#include "cctki_Cache.h"
+
+/********************************************************************
+ *********************     Local Data   *****************************
+ ********************************************************************/
+
+/* Structure to hold cache information. */
+static struct s_cache_data
+{
+  unsigned long cacheline_bytes;
+  unsigned long cache_size;
+} cache_data;
+
+/* The number of times the size of the cache has been set. */
+static int cache_set = 0;
+
+/********************************************************************
+ *********************     External Routines   **********************
+ ********************************************************************/
+
+ /*@@
+   @routine    CCTK_CacheMalloc
+   @date       Tue Nov 30 08:13:03 1999
+   @author     Tom Goodale
+   @desc 
+   Allocates memory aligned on the 'index'ed cache line.
+   @enddesc 
+   @calls     
+   @calledby   
+   @history 
+ 
+   @endhistory 
+
+   @var     index
+   @vdesc   cache line index
+   @vtype   int
+   @vio     in
+   @vcomment 
+   The cache line to align on
+   @endvar 
+   @var     size
+   @vdesc   the amount of memory to allocate
+   @vtype   unsigned long
+   @vio     in
+   @vcomment 
+   This is the size (in bytes) of memory to allocate.
+   @endvar 
+   @var     realstart
+   @vdesc   The real starting address of the memory
+   @vtype   void **
+   @vio     out
+   @vcomment 
+   On return holds the actual starting address of the memory.
+   @endvar 
+
+   @returntype void *
+   @returndesc
+   The cache-aligned memory address.
+   @endreturndesc
+
+@@*/
+void *CCTK_CacheMalloc(unsigned index, 
+                       unsigned long size, 
+                       void **realstart)
+{
+  char *retval;
+  char *data;
+  unsigned long cacheline_bytes;
+  unsigned long cache_size;
+  unsigned long offset;
+  unsigned long initial_index;
+  unsigned long pad;
+
+  if(CCTKi_CacheDataGet(&cacheline_bytes, &cache_size))
+  {
+    cacheline_bytes = 0;
+    cache_size = 0;
+  }
+
+  data = malloc(size+cache_size);
+
+  if(data)
+  {
+    if(cache_size)
+    {
+      /* Find how far this memory is into a cache line */
+      offset = ((unsigned long)data%cache_size)%cacheline_bytes;
+      /* Find which cache line in the cache it is in */
+      initial_index = ((unsigned long)data%cache_size)/cacheline_bytes;
+      
+      pad = ((index-initial_index)*cacheline_bytes + cacheline_bytes - offset)%cache_size;
+    }
+    else
+    {
+      pad = 0;
+    }
+
+    retval = data + pad;
+  }
+  else
+  {
+    retval = NULL;
+  }
+
+  *realstart = (void *)data;
+
+  return (void *)retval;
+}
+
+/********************************************************************
+ *********************   CCTK Local Routines   **********************
+ ********************************************************************/
+
+ /*@@
+   @routine    CCTKi_CacheDataSet
+   @date       Tue Nov 30 08:12:05 1999
+   @author     Tom Goodale
+   @desc 
+   Stores the sizes of the cache.
+   @enddesc 
+   @calls     
+   @calledby   
+   @history 
+ 
+   @endhistory 
+
+   @var     cacheline_bytes
+   @vdesc   The size of a cacheline
+   @vtype   unsigned long
+   @vio     in
+   @vcomment 
+   Number of bytes in a cache line.
+   @endvar 
+   @var     cache_size
+   @vdesc   Size of the cache
+   @vtype   unsigned long
+   @vio     in
+   @vcomment 
+   The size (in bytes) of the cache.
+   @endvar 
+
+   @returntype int
+   @returndesc
+   The number of times this function has been called before.
+   @endreturndesc
+
+@@*/
+int CCTKi_CacheDataSet(unsigned long cacheline_bytes,
+                       unsigned long cache_size)
+{
+  cache_data.cacheline_bytes = cacheline_bytes;
+  cache_data.cache_size      = cache_size;
+
+  cache_set++;
+
+  return cache_set-1;
+}
+
+ /*@@
+   @routine    CCTKi_CacheDataGet
+   @date       Tue Nov 30 08:12:41 1999
+   @author     Tom Goodale
+   @desc 
+   Gets the sizes of the cache.
+   @enddesc 
+   @calls     
+   @calledby   
+   @history 
+ 
+   @endhistory 
+
+   @var     cacheline_bytes
+   @vdesc   The size of a cacheline
+   @vtype   unsigned long *
+   @vio     out
+   @vcomment 
+   Number of bytes in a cache line.
+   @endvar 
+   @var     cache_size
+   @vdesc   Size of the cache
+   @vtype   unsigned long *
+   @vio     out
+   @vcomment 
+   The size (in bytes) of the cache.
+   @endvar 
+
+   @returntype int
+   @returndesc
+   0  - success
+   -1 - cache sizes have not been set yet.
+   @endreturndesc
+
+@@*/
+int CCTKi_CacheDataGet(unsigned long *cacheline_bytes,
+                       unsigned long *cache_size)
+{
+  int retcode;
+
+  if(cache_set)
+  {
+    *cacheline_bytes = cache_data.cacheline_bytes;
+    *cache_size      = cache_data.cache_size;
+    retcode = 0;
+  }
+  else
+  {
+    retcode = -1;
+  }
+
+  return retcode; 
+}
+
