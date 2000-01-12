28b1ba98a0ff6b4eb2ce63612f84c2569e64885e
Gabrielle Allen <gdallen@illinois.edu>
2000-01-12 16:18:47 +0000

    Remove pointer warning on origin
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1240 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctk.h
+++ src/include/cctk.h
@@ -125,6 +125,7 @@
             cctk_dummy_real=0;\
             cctk_dummy_real+=0;\
             cctk_dummy_pointer=NULL;\
+            cctk_dummy_pointer=(CCTK_REAL *)cctk_dummy_pointer;\
             cctk_dummy_int=cctk_dim;\
             cctk_dummy_pointer=(void *)cctk_gsh;\
             cctk_dummy_pointer=(void *)cctk_lsh;\
