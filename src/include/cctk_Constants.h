9f9c824fc33eef97ec50b7aa6eef0a790b268227
Tom Goodale <goodale@cct.lsu.edu>
2000-01-21 17:11:32 +0000

    Added some more variable type constants.
    Tom
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1278 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctk_Constants.h
+++ src/include/cctk_Constants.h
@@ -5,24 +5,27 @@
    @desc 
    Constants used by Cactus
    @enddesc
-   @version $Id$
+   @version $Header$
  @@*/
 
 #ifndef _CCTK_CONSTANTS_H_
 #define _CCTK_CONSTANTS_H_
 
-#define CCTK_VARIABLE_CHAR    1
-#define CCTK_VARIABLE_INT     2
-#define CCTK_VARIABLE_INT2    3
-#define CCTK_VARIABLE_INT4    4
-#define CCTK_VARIABLE_INT8    5
-#define CCTK_VARIABLE_REAL    6
-#define CCTK_VARIABLE_REAL4   7
-#define CCTK_VARIABLE_REAL8   8
-#define CCTK_VARIABLE_REAL16  9
-#define CCTK_VARIABLE_COMPLEX 10
-#define CCTK_VARIABLE_STRING  11
-
+#define CCTK_VARIABLE_VOID       0
+#define CCTK_VARIABLE_CHAR       1
+#define CCTK_VARIABLE_INT        2
+#define CCTK_VARIABLE_INT2       3
+#define CCTK_VARIABLE_INT4       4
+#define CCTK_VARIABLE_INT8       5
+#define CCTK_VARIABLE_REAL       6
+#define CCTK_VARIABLE_REAL4      7
+#define CCTK_VARIABLE_REAL8      8
+#define CCTK_VARIABLE_REAL16     9
+#define CCTK_VARIABLE_COMPLEX   10
+#define CCTK_VARIABLE_COMPLEX8  11
+#define CCTK_VARIABLE_COMPLEX16 12
+#define CCTK_VARIABLE_COMPLEX32 13
+#define CCTK_VARIABLE_STRING    14
 
 /* stagger flags: yes/no, number of staggerings: 3 */
 #define CCTK_NO_STAGGER    0
@@ -72,6 +75,5 @@
 #define CCTK_STAG_PPP 27
 
 
-
-#endif
+#endif /* _CCTK_CONSTANTS_ */
 
