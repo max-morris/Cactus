d5e7bdc217d3bc55b89846a2f094c8c8d3e7f60a
Tom Goodale <goodale@cct.lsu.edu>
2000-01-27 17:13:59 +0000

    Split cctki_Schedule.h in two
    cctki_Schedule.h just contains prototypes and stuff from the schedule directory.
    
    cctki_ScheduleBindings.h contains routines from the rest of cactus.
    
    Tom
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1308 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctki_Schedule.h
+++ src/include/cctki_Schedule.h
@@ -3,7 +3,8 @@
    @date      Wed Sep 15 22:49:24 1999
    @author    Tom Goodale
    @desc 
-   Schedule stuff which can be seen by the rest of the flesh.
+   Schedule stuff from the core schedule subsystem
+   which can be seen by the rest of the flesh.
    @enddesc 
    @version $Header$
  @@*/
@@ -27,7 +28,8 @@ typedef struct T_SCHED_MODIFIER
 
 
 #ifdef __cplusplus
-extern "C" {
+extern "C" 
+{
 #endif
 
 /* Routines to create items */
@@ -35,63 +37,13 @@ t_sched_modifier *CCTKi_ScheduleAddModifier(t_sched_modifier *orig,
                                            const char *modifier, 
                                            const char *argument);
 
-int CCTKi_ScheduleFunction
-  (
-   void *function,
-   const char *name,
-   const char *thorn,
-   const char *implementation,
-   const char *description,
-   const char *where,
-   const char *language,
-   int n_mem_groups,
-   int n_comm_groups,
-   int n_trigger_groups,
-   int n_before,
-   int n_after,
-   int n_while,
-   ...
-  );
-
-int CCTKi_ScheduleGroup
-  (
-   const char *name,
-   const char *thorn,
-   const char *implementation,
-   const char *description,
-   const char *where,
-   int n_mem_groups,
-   int n_comm_groups,
-   int n_trigger_groups,
-   int n_before,
-   int n_after,
-   int n_while,
-   ...
-  );
-
-int CCTKi_ScheduleGroupStorage(const char *group);
-
-int CCTKi_ScheduleGroupComm(const char *group);
-
-int CCTKi_ScheduleTraverse(const char *where, 
-			   void *GH,   
-			   int (*calling_function)(void *, void *, void *));
-
-int CCTKi_ScheduleGHInit(void *GH);
 
 
-int CCTKi_DoScheduleFunction
-  (
-   const char *gname, 
-   const char *fname, 
-   void *func, 
-   t_sched_modifier *modifiers, 
-   void *attributes
-  );
-
-t_sched_modifier *CCTKi_DoScheduleAddModifier(t_sched_modifier *orig,
-					      const char *modifier, 
-					      const char *argument);
+int CCTKi_DoScheduleFunction(const char *gname, 
+                             const char *fname, 
+                             void *func, 
+                             t_sched_modifier *modifiers, 
+                             void *attributes);
 
 int CCTKi_DoScheduleGroup
   (
