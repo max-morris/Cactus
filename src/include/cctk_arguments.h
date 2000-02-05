9c5d4072bce383d92431e1c07b7733aad154ea26
Gabrielle Allen <gdallen@illinois.edu>
2000-02-05 14:28:51 +0000

    Changes to add #ifndefs
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1366 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctk_arguments.h
+++ src/include/cctk_arguments.h
@@ -1,1 +1,15 @@
+ /*@@
+   @header    cctk_arguments.h
+   @date      Sat Feb 5 2000
+   @author    Gabrielle Allen
+   @desc 
+   cctk_arguments.h is deprecated now. This file is temporary.
+   @enddesc 
+ @@*/
+
+#ifndef _CCTK_OLDARGUMENTS_H_
+#define _CCTK_OLDARGUMENTS_H_
+
 #include "cctk_Arguments.h"
+
+#endif
--- src/include/cctk_parameters.h
+++ src/include/cctk_parameters.h
@@ -1,1 +1,21 @@
-#include "cctk_Parameters.h"
+ /*@@
+   @header    cctk_parameters.h
+   @date      Sat Feb 5 2000
+   @author    Gabrielle Allen
+   @desc 
+   cctk_parameters.h is deprecated now. This file is temporary.
+   @enddesc 
+ @@*/
+
+#ifndef _CCTK_PARAMETERS_H_
+#define _CCTK_PARAMETERS_H_
+
+#ifdef CCODE
+#include "CParameters.h"
+#endif
+
+#ifdef FCODE
+#include "FParameters.h"
+#endif
+
+#endif
