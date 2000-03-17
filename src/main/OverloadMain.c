9af360b77ce41d39bf8a5b6b248503654157b9c4
Gerd Lanfermann <lanfer@aei.mpg.de>
2000-03-17 16:05:55 +0000

    removing superflous define
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1476 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/main/OverloadMain.c
+++ src/main/OverloadMain.c
@@ -24,7 +24,6 @@ static char *rcsid="$Header$";
 #define CCTKi_DummyInitialise  CactusDefaultInitialise
 #define CCTKi_DummyEvolve      CactusDefaultEvolve
 #define CCTKi_DummyShutdown    CactusDefaultShutdown
-#define CCTKi_DummyExit        CactusDefaultExit
 
 #include "MainOverloadables.h"
 
@@ -32,7 +31,6 @@ static char *rcsid="$Header$";
 #undef CCTKi_DummyInitialise 
 #undef CCTKi_DummyEvolve     
 #undef CCTKi_DummyShutdown   
-#undef CCTKi_DummyExit        CactusDefaultExit
 
 #undef OVERLOADABLE
 
