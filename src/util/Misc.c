a64b9c7e066671d1fab42a71fddbb4ace5dde7e2
Tom Goodale <goodale@cct.lsu.edu>
2000-04-04 09:49:17 +0000

    Added back the line to in CCTK_FortranString which does a ONE_FORTSTRING_CREATE.
    This will produce compiler warnings about unused variables on most platforms
    but is necessary for the T3E.
    
    Tom
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1512 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/util/Misc.c
+++ src/util/Misc.c
@@ -1007,6 +1007,7 @@ void FORTRAN_NAME(CCTK_FortranString)(CCTK_INT *nchar,
                                       ONE_FORTSTRING_ARG)
 {
   int i;
+  ONE_FORTSTRING_CREATE(fstring)
   ONE_FORTSTRING_PTR(fptr)
 
   if (strlen(*cstring) > cctk_strlen1) 
