5d722cfe5c57f3dd512e4a37adefb78762865f67
Gabrielle Allen <gdallen@illinois.edu>
2000-04-17 07:32:54 +0000

    Think there was a typo in the macro for removing rcsid warnings.
    Also, at the moment cctk_Flesh.h has to be included in any file using the macro. This won't work for the thorns,
    perhaps it also needs to be added to cctk.h /
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1547 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctk_Flesh.h
+++ src/include/cctk_Flesh.h
@@ -34,7 +34,7 @@ typedef struct
 
 #else
 
-#define CCTK_FILEVERSION(file) extern "C" const char *CCTKi_version_##file(void) { return rcsid; }
+#define CCTK_FILEVERSION(file) const char *CCTKi_version_##file(void) { return rcsid; }
 
 #endif
 
