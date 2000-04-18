ad2e9df658051c65dad8864059672d7d4922cf12
Tom Goodale <goodale@cct.lsu.edu>
2000-04-18 14:02:07 +0000

    Suppress warnings (in C compilation) for timelevel arguments.
    Make PASS_..._C2F compatible with the corresponding prototypes.
    
    Tom
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1577 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- lib/sbin/GridFuncStuff.pl
+++ lib/sbin/GridFuncStuff.pl
@@ -555,51 +555,7 @@ sub CreateCArgumentUses
 	  $suffix .= "_p";
 	}
 
-	if($1 eq "CHAR")
-	{
-	  push(@declarations, "cctk_dummy_pointer =$argument;");
-	}
-	elsif ($1 eq REAL)
-	{
-	  push(@declarations, "cctk_dummy_pointer =$argument;");
-	}
-	elsif ($1 eq REAL4)
-	{
-	  push(@declarations, "cctk_dummy_pointer =$argument;");
-	}
-	elsif ($1 eq REAL8)
-	{
-	  push(@declarations, "cctk_dummy_pointer =$argument;");
-	}
-	elsif ($1 eq REAL16)
-	{
-	  push(@declarations, "cctk_dummy_pointer=$argument;");
-	}
-	elsif ($1 eq COMPLEX)
-	{
-	  push(@declarations, "cctk_dummy_pointer =$argument;");
-	}
-	elsif ($1 eq INT)
-	{
-	  push(@declarations, "cctk_dummy_pointer=$argument;");
-	}
-	elsif ($1 eq INT2)
-	{
-	  push(@declarations, "cctk_dummy_pointer=$argument;");
-	}
-	elsif ($1 eq INT4)
-	{
-	  push(@declarations, "cctk_dummy_pointer=$argument;");
-	}
-	elsif ($1 eq INT8)
-	{
-	  push(@declarations, "cctk_dummy_pointer=$argument;");
-	}
-	else
-	{
-	  $message = "Unknown argument type $1";
-	  &CST_error(0,$message,__LINE__,__FILE__);
-	}
+	push(@declarations, "cctk_dummy_pointer =$argument$suffix;");
 
       }
     }
@@ -897,7 +853,7 @@ sub CreateCArgumentList
   {
     if($arguments{$argument} =~ m:STORAGESIZE\(([^,]*),\s*(\d+):)
     {
-      $arglist .= "$sep"."(int *)(CCTK_STORAGESIZE(xGH, $2, \"$1\"))";
+      $arglist .= "$sep"."(const int *)(CCTK_STORAGESIZE(xGH, $2, \"$1\"))";
       $sep = ",";
     }
   }
