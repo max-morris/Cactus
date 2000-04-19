6f4735f0d75c8032c0804f07c2e1c97beee0aba0
Gabrielle Allen <gdallen@illinois.edu>
2000-04-19 11:09:26 +0000

    Set warning level to 1 for failing to recover from parameters. Suspect that if noone even tries to recover parameters an error code is returned. Need to fix this properly.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1589 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/main/InitialiseCactus.c
+++ src/main/InitialiseCactus.c
@@ -106,7 +106,7 @@ int CCTKi_InitialiseScheduler(tFleshConfig *ConfigData)
   CCTKi_SetParameterSetMask(PARAMETER_RECOVERY_IN);
 
   if(CCTKi_BindingsParameterRecoveryInitialise() != 0)
-    CCTK_WARN(0, "Failed to recover parameters");
+    CCTK_WARN(1, "Failed to recover parameters");
 
   CCTKi_SetParameterSetMask(PARAMETER_RECOVERY_POST);
 
