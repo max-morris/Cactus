bc4ea483c9c32f75780ee2a0b98efa39faeba6f8
Gerd Lanfermann <lanfer@aei.mpg.de>
2000-05-03 12:19:56 +0000

    adding prototype CCTK_GroupDimFromVarI(int vi)
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1625 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctk_Groups.h
+++ src/include/cctk_Groups.h
@@ -43,7 +43,7 @@ char      *CCTK_FullName(int var);
 
 int        CCTK_GroupData(int group, cGroup *gp);
 int        CCTK_GroupDimI(int group);
-int        CCTK_GroupDimFromVarI(int vi)
+int        CCTK_GroupDimFromVarI(int vi);
 CCTK_INT **CCTK_GroupGhostsizesI(int group);
 int        CCTK_GroupIndex(const char *groupname);
 int        CCTK_GroupIndexFromVar(const char *var);
