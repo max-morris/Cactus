02283beb6c4aa17af41f22a9e8242147007d862d
Gabrielle Allen <gdallen@illinois.edu>
2000-06-22 21:24:41 +0000

    Prototypes and structure for new array querying functions
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1694 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctk_GroupsOnGH.h
+++ src/include/cctk_GroupsOnGH.h
@@ -11,6 +11,17 @@
 #ifndef _CCTK_GROUPSONGH_H_
 #define _CCTK_GROUPSONGH_H_
 
+typedef struct GROUPDYNAMICDATA
+{
+  int dim;
+  const int *gsh;
+  const int *lsh;
+  const int *lbnd;
+  const int *ubnd;
+  const int *bbox;
+  const int *nghostzones;
+} cGroupDynamicData;
+
 /* Prototypes */
 
 #ifdef __cplusplus 
@@ -28,8 +39,37 @@ int CCTK_DisableGroupCommI(void *GH, int group);
 int CCTK_EnableGroupStorageI(void *GH, int group);
 int CCTK_EnableGroupCommI(void *GH, int group);
 
+int CCTK_GrouplbndGN(cGH *GH, int dim, int *lbnd, const char *groupname);
+int CCTK_GrouplbndVN(cGH *GH, int dim, int *lbnd, const char *varname);
+int CCTK_GrouplbndGI(cGH *GH, int dim, int *lbnd, int groupindex);
+int CCTK_GrouplbndVI(cGH *GH, int dim, int *lbnd, int varindex);
+
+int CCTK_GroupubndGN(cGH *GH, int dim, int *ubnd, const char *groupname);
+int CCTK_GroupubndVN(cGH *GH, int dim, int *ubnd, const char *varname);
+int CCTK_GroupubndGI(cGH *GH, int dim, int *ubnd, int groupindex);
+int CCTK_GroupubndVI(cGH *GH, int dim, int *ubnd, int varindex);
+
+int CCTK_GrouplshGN(cGH *GH, int dim, int *lsh, const char *groupname);
+int CCTK_GrouplshVN(cGH *GH, int dim, int *lsh, const char *varname);
+int CCTK_GrouplshGI(cGH *GH, int dim, int *lsh, int groupindex);
+int CCTK_GrouplshVI(cGH *GH, int dim, int *lsh, int varindex);
+
+int CCTK_GroupgshGN(cGH *GH, int dim, int *gsh, const char *groupname);
+int CCTK_GroupgshVN(cGH *GH, int dim, int *gsh, const char *varname);
+int CCTK_GroupgshGI(cGH *GH, int dim, int *gsh, int groupindex);
+int CCTK_GroupgshVI(cGH *GH, int dim, int *gsh, int varindex);
+
+int CCTK_GroupnghostzonesGN(cGH *GH, int dim, int *nghostzones, 
+			    const char *groupname);
+int CCTK_GroupnghostzonesVN(cGH *GH, int dim, int *nghostzones, 
+			    const char *varname);
+int CCTK_GroupnghostzonesGI(cGH *GH, int dim, int *nghostzones, 
+			    int groupindex);
+int CCTK_GroupnghostzonesVI(cGH *GH, int dim, int *nghostzones, 
+			    int varindex);
+
 #ifdef __cplusplus 
 }
 #endif
-
+ 
 #endif
