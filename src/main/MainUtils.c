bf141001a35ed581002191e8de8d6a691e90d241
Gabrielle Allen <gdallen@illinois.edu>
2000-09-19 23:41:46 +0000

    Added CCTK_RunTitle which provides a string giving a description of the simulation. This might change name in the near future
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@1828 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/main/MainUtils.c
+++ src/main/MainUtils.c
@@ -8,6 +8,20 @@
    @version $Header$
  @@*/
 
+#include <string.h>
+
+#include "cctk_Flesh.h"
+#include "cctk_Misc.h"
+#include "cctk_Schedule.h"
+#include "cctk_Parameter.h"
+
+static char *rcsid = "$Header$";
+
+static const char *startuptime = "How do I do this";
+
+CCTK_FILEVERSION(main_MainUtils_c)
+
+
 /* the iteration counter used in the evolution loop */
 static int iteration = 0;
 
@@ -46,5 +60,47 @@ int CCTK_SetMainLoopIndex (int main_loop_index)
 int CCTK_MainLoopIndex (void)
 {
   return (iteration);
+}
+
+ /*@@
+   @routine    CCTK_RunTitle
+   @date       Sun Sep 17 2000
+   @author     Gabrielle Allen
+   @desc 
+   Returns the simulation description
+   @enddesc 
+   @calls     
+   @calledby   
+
+@@*/
+int CCTK_RunTitle(int len, char *title)
+{
+  int retval; 
+  int param_type;
+  const char *cctk_title;
+
+  cctk_title = (*(const char **)
+		CCTK_ParameterGet("cctk_run_title",
+				  "Cactus",
+				  &param_type));
+  
+  if (cctk_title)
+  {
+    if (CCTK_Equals(cctk_title,""))
+    {
+      strcpy(title,"Cactus Simulation");
+    }
+    else
+    {
+      strncpy(title,cctk_title,len-1);
+    }
+    retval = strlen(title);
+    retval=retval > len ? 0 : retval;
+  }
+  else
+  {
+    retval = -1;
+  }
+  return retval;
 }
 
