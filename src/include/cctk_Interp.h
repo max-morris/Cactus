a6ae0fc6dce41ed5bef3e9bc57499668be40e95c
Thomas Radke <tradke@aei.mpg.de>
2001-02-14 14:49:57 +0000

    Added prototypes for new interpolation API routines CCTK_InterpGV() and CCTK_InterpLocal().
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@2021 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctk_Interp.h
+++ src/include/cctk_Interp.h
@@ -20,6 +20,62 @@
 extern "C" {
 #endif
 
+/* prototype for interpolation operator routine
+   working on a list of grid variables */
+typedef int (*cInterpOperatorGV) (cGH *GH,
+                                  const char *coord_system,
+                                  int num_points,
+                                  int num_in_array_indices,
+                                  int num_out_arrays,
+                                  void *interp_coord_arrays[],
+                                  int interp_coord_array_types[],
+                                  int in_array_indices[],
+                                  void *out_arrays[],
+                                  int out_array_types[]);
+
+/* prototype for interpolation operator routine
+   working on local arrays */
+typedef int (*cInterpOperatorLocal) (cGH *GH,
+                                     int num_points,
+                                     int num_dims,
+                                     int num_in_arrays,
+                                     int num_out_arrays,
+                                     int coord_dims[],
+                                     void *coord_arrays[],
+                                     int coord_array_types[],
+                                     void *interp_coord_arrays[],
+                                     int interp_coord_array_types[],
+                                     void *in_arrays[],
+                                     int in_array_types[],
+                                     void *out_arrays[],
+                                     int out_array_types[]);
+
+int CCTK_InterpHandle (const char *interp);
+
+int CCTK_InterpRegisterOperatorGV (cInterpOperatorGV operator_GV,
+                                   const char *name);
+
+int CCTK_InterpRegisterOperatorLocal (cInterpOperatorLocal operator_local,
+                                      const char *name);
+
+int CCTK_InterpGV (cGH *GH,
+                   int operator_handle,
+                   int coord_system_handle,
+                   int num_points,
+                   int num_in_array_indices,
+                   int num_out_arrays,
+                   ...);
+
+int CCTK_InterpLocal (cGH *GH,
+                      int operator_handle,
+                      int num_points,
+                      int num_dims,
+                      int num_in_arrays,
+                      int num_out_arrays,
+                      ...);
+
+
+/* depricated functions */
 #define INTERP_REGISTER_ARGLIST  \
                          cGH *,  \
                          int,    \
@@ -44,10 +100,8 @@ int CCTK_InterpArray(cGH *GH,
                 int nOutFields,
                 ...);
 
-int CCTK_InterpHandle(const char *interp);
-
-int CCTK_InterpRegisterOperator(int (*function)(INTERP_REGISTER_ARGLIST),
-                                const char *name);
+int CCTK_InterpRegisterOperator (int (*function)(INTERP_REGISTER_ARGLIST),
+                                 const char *name);
 
 int CCTK_Interp(cGH *GH,
                  int operation_handle,
