5e7546a39d74a64baae8b1072dacba2c410d393a
Gabrielle Allen <gdallen@illinois.edu>
2001-02-24 17:17:55 +0000

    Adding missing function prototype
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@2055 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/util_String.h
+++ src/include/util_String.h
@@ -16,10 +16,22 @@ extern "C"
 {
 #endif
 
-const char *Util_StrSep(const char **stringp, const char *delim);
-int Util_SplitString(char **before, char **after, const char *string, const char *sep);
+const char *Util_StrSep(const char **stringp, 
+                        const char *delim);
+
+int Util_SplitString(char **before, 
+                     char **after, 
+                     const char *string, 
+                     const char *sep);
+
+int Util_SplitFilename(char **dir, 
+                       char **file, 
+                       const char *string);
+
 char *Util_Strdup(const char *s);
-int Util_StrCmpi(const char *string1, const char *string2);
+
+int Util_StrCmpi(const char *string1, 
+                 const char *string2);
 
 #ifdef __cplusplus
 }
