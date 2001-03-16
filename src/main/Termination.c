01cb9465941a73c0c6a11d3c305075cacdd4ba2f
Gabrielle Allen <gdallen@illinois.edu>
2001-03-16 13:52:23 +0000

    Routines for handling termination conditions, only basic stuff for now.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@2079 17b73243-c579-4c4c-a9d2-2d5706c11dac

---
+++ src/main/Termination.c
@@ -0,0 +1,85 @@
+ /*@@
+   @file      Termination.c
+   @date      Thu March 15 7pm
+   @author    Gabrielle Allen
+   @desc 
+   Termination conditions 
+   @enddesc 
+   @version $Header$
+ @@*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "cctk_Flesh.h"
+#include "cctk_Parameter.h"
+
+#include "cctk_Main.h"
+#include "cctk_IO.h"
+
+static char *rcsid="$Header$";
+
+CCTK_FILEVERSION(main_Termination_c)
+
+/********************************************************************
+ *********************     Local Data Types   ***********************
+ ********************************************************************/
+
+/********************************************************************
+ *********************     Local Data   *****************************
+ ********************************************************************/
+
+static int termination_reached = 0;
+
+/********************************************************************
+ ********************* Local Routine Prototypes *********************
+ ********************************************************************/
+
+/********************************************************************
+ *********************     External Routines   **********************
+ ********************************************************************/
+
+ /*@@
+   @routine    CCTK_TerminationReached(cGH *)
+   @date       Thu March 15 7pm
+   @author     Gabrielle Allen
+   @desc 
+   Returns true if Cactus will terminate on the next iteration
+   @enddesc 
+   @calls     
+   @calledby   
+   @history 
+   @hdate @hauthor 
+   @hdesc 
+   @endhistory 
+
+@@*/
+int CCTK_TerminationReached(cGH *GH)
+{
+  return termination_reached;
+}
+  
+ /*@@
+   @routine    CCTK_TerminateNext(cGH *)
+   @date       Thu March 15 7pm
+   @author     Gabrielle Allen
+   @desc 
+   Sets termination for next iteration
+   @enddesc 
+   @calls     
+   @calledby   
+   @history 
+   @hdate @hauthor 
+   @hdesc 
+   @endhistory 
+
+@@*/
+int CCTK_TerminateNext(cGH *GH)
+{
+  termination_reached = 1;
+}
+  
+/********************************************************************
+ *********************     Local Routines   *************************
+ ********************************************************************/
+
