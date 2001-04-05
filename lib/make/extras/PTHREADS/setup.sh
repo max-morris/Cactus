b167d964d5d8595afba9f05b26dcba5b73293ef1
Thomas Radke <tradke@aei.mpg.de>
2001-04-05 16:44:21 +0000

    Cannot modify CFLAGS at this stage. So rather put defines necessary for PTHREADS into cctk_Extradefs.h.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@2095 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- lib/make/extras/PTHREADS/setup.sh
+++ lib/make/extras/PTHREADS/setup.sh
@@ -18,12 +18,14 @@ echo "Configuring with PTHREADS"
 
 CCTK_WriteLine cctk_Extradefs.h "#define CCTK_PTHREADS 1"
 
-# the PTHREAD_xxx variables are set by configure
-CCTK_WriteLine make.extra.defn "PTHREADS_CFLAGS   = $PTHREADS_CFLAGS"
-CCTK_WriteLine make.extra.defn "PTHREADS_CXXFLAGS = $PTHREADS_CXXFLAGS"
-CCTK_WriteLine make.extra.defn "PTHREADS_LIBS     = $PTHREAD_LIBS"
+# the PTHREADS_DEFINE variable is set by configure
+if test "X$PTHREADS_DEFINE" != "X"; then
+  CCTK_WriteLine cctk_Extradefs.h "#define $PTHREADS_DEFINE 1"
+fi
+
+
+# the PTHREAD_LIBS variable is set by configure
+CCTK_WriteLine make.extra.defn "PTHREADS_LIBS = $PTHREAD_LIBS"
 CCTK_WriteLine make.extra.defn ""
 CCTK_WriteLine make.extra.defn ""
-CCTK_WriteLine make.extra.defn 'CFLAGS   += $(PTHREADS_CFLAGS)'
-CCTK_WriteLine make.extra.defn 'CXXFLAGS += $(PTHREADS_CXXFLAGS)'
 CCTK_WriteLine make.extra.defn 'LIBS     += $(PTHREADS_LIBS)'
