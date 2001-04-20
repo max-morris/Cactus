11816c76f2b2e954c8a532bf6caec70251b66c10
Thomas Radke <tradke@aei.mpg.de>
2001-04-20 21:38:29 +0000

    Added missing prototypes for CCTKi_ routines.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@2130 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctki_WarnLevel.h
+++ src/include/cctki_WarnLevel.h
@@ -2,24 +2,41 @@
    @header    cctki_WarnLevel.h
    @date      Wed Feb 17 00:53:55 1999
    @author    Tom Goodale
-   @desc 
-   Header for the internal warning functions.
-   @enddesc 
+   @desc
+              Header for the internal warning functions.
+   @enddesc
+   @version   $Id$
  @@*/
 
 #ifndef _CCTKI_WARNLEVEL_H_
 #define _CCTKI_WARNLEVEL_H_
 
-#ifdef __cplusplus 
-extern "C" {
+#ifdef __cplusplus
+extern "C"
+{
 #endif
 
-int CCTKi_SetWarnLevel(int level);
-void CCTKi_FinaliseParamWarn(void);
-int CCTKi_SetErrorLevel(int level);
-void CCTKi_NotYetImplemented(const char *message);
-  
-#ifdef __cplusplus 
+int  CCTKi_SetWarnLevel (int level);
+int  CCTKi_SetParameterLevel (int level);
+int  CCTKi_SetErrorLevel (int level);
+void CCTKi_FinaliseParamWarn (void);
+void CCTKi_NotYetImplemented (const char *message);
+void CCTKi_ExpectError (int in,
+                        int err,
+                        int warnonerr,
+                        int line,
+                        const char *file,
+                        const char *thorn,
+                        const char *message);
+void CCTKi_ExpectOK (int in,
+                     int ok,
+                     int warnonerr,
+                     int line,
+                     const char *file,
+                     const char *thorn,
+                     const char *message);
+
+#ifdef __cplusplus
 }
 #endif
 
