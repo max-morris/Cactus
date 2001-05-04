96d7bdda96542023d19cf4dcf5470864f6561457
Thomas Radke <tradke@aei.mpg.de>
2001-05-04 18:48:11 +0000

    In CCTK_SyncGroupsI(), changed an 'int *' parameter into a 'const int *'.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@2142 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctk_Sync.h
+++ src/include/cctk_Sync.h
@@ -2,32 +2,29 @@
    @header    cctk_Sync.h
    @date      Thu Jan 27 18:04:12 2000
    @author    Tom Goodale
-   @desc 
-   Various synchronisation functions.
+   @desc
+              Various CCTK synchronisation functions.
    @enddesc
-   @version $Header$
+   @version   $Header$
  @@*/
 
 #ifndef _CCTK_SYNC_H_
 #define _CCTK_SYNC_H_
 
 #ifdef __cplusplus
-extern "C" 
+extern "C"
 {
 #endif
 
-void CCTK_SyncGroupI(cGH *GH, 
-                     int groupi);
- 
-void CCTK_SyncGroupWithVar(cGH *GH, 
-                           const char *varn);
-
-void CCTK_SyncGroupWithVarI(cGH *GH, 
-                            int vari);
-
-int CCTK_SyncGroupsI(cGH *GH, 
-                     int n_groups, 
-                     int *groups);
+void CCTK_SyncGroupI (cGH *GH,
+                      int group);
+void CCTK_SyncGroupWithVar (cGH *GH,
+                            const char *varname);
+void CCTK_SyncGroupWithVarI (cGH *GH,
+                             int var);
+int CCTK_SyncGroupsI (cGH *GH,
+                      int n_groups,
+                      const int *groups);
 
 #ifdef __cplusplus
 }
