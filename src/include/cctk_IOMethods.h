29cc29ca9dd779235c01c3abda660a8717effb78
Thomas Radke <tradke@aei.mpg.de>
2001-10-24 11:59:39 +0000

    Include a structure definition into 'extern "C" {' just to be sure that C bindings are used for it.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@2429 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctk_IOMethods.h
+++ src/include/cctk_IOMethods.h
@@ -1,15 +1,19 @@
  /*@@
    @header    cctk_IOMethods.h
-   @date      
-   @author    
-   @desc 
-   header file for handling IO methods
-   @enddesc 
-   @version $Id$
+   @date      1999/07/30
+   @author    Gabrielle Allen
+   @desc
+              header file for handling IO methods
+   @enddesc
+   @version   $Version: cctk_IOMethods.h,v 1.3 2001/10/23 15:40:08 allen Exp $
  @@*/
 
 #ifndef _CCTK_IOMETHODS_H_
 #define _CCTK_IOMETHODS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 struct IOMethod
 {
@@ -18,13 +22,9 @@ struct IOMethod
   int    (*OutputVarAs)(cGH *, const char *, const char *);
   int    (*TriggerOutput)(cGH *, int);
   int    (*TimeToOutput)(cGH *,int);
-};
+}
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#define CCTK_RegisterIOMethod(a) CCTKi_RegisterIOMethod(CCTK_THORNSTRING,a)
+#define CCTK_RegisterIOMethod(a) CCTKi_RegisterIOMethod(CCTK_THORNSTRING, a)
 int CCTKi_RegisterIOMethod(const char *thorn, const char *name);
 
 int CCTK_RegisterIOMethodOutputGH(int handle, int (*func)(cGH *));
