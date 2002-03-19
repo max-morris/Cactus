0ec40081ec870d8c0475af2d6fdae092d0215a58
Thomas Radke <tradke@aei.mpg.de>
2002-03-19 15:58:11 +0000

    Store the information whether we recover from a checkpoint or not (as returned by CCTKi_BindingsParameterRecoveryInitialise()) in the tFleshConfig structure.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@2652 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/main/InitialiseCactus.c
+++ src/main/InitialiseCactus.c
@@ -2,14 +2,13 @@
    @file      InitialiseCactus.c
    @date      Fri Sep 18 14:04:02 1998
    @author    Tom Goodale
-   @desc 
+   @desc
               Responsible for doing any cactus specific initialisations
-   @enddesc 
+   @enddesc
    @version   $Id$
  @@*/
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 
 #include "cctk_Flesh.h"
@@ -28,47 +27,30 @@ static const char *rcsid = "$Header$";
 CCTK_FILEVERSION(main_InitialiseCactus_c)
 
 /********************************************************************
- *********************     Local Data Types   ***********************
- ********************************************************************/
-
-/********************************************************************
  ********************* Local Routine Prototypes *********************
  ********************************************************************/
-
 static int CCTKi_InitialiseScheduler (tFleshConfig *ConfigData);
 
 /********************************************************************
  ********************* Other Routine Prototypes *********************
  ********************************************************************/
-
 int CCTKBindings_RegisterThornFunctions (void);
 int CCTKi_InitialiseSubsystemDefaults (void);
-int CCTKi_ProcessEnvironment (int *argc, char ***argv,tFleshConfig *ConfigData);
 int CCTKi_BindingsParameterRecoveryInitialise (void);
-
-int CCTKi_ProcessCommandLine (int *inargc, char ***inargv, tFleshConfig *ConfigData);
-int CCTKi_ProcessEnvironment (int *argc, char ***argv,tFleshConfig *ConfigData);
 
 /********************************************************************
  *********************     Local Data   *****************************
  ********************************************************************/
-
-#define CCTK_PRINTSEPARATOR \
-  printf("--------------------------------------------------------------------------------\n");
-
 static time_t startuptime;
 
-/********************************************************************
- *********************     External Routines   **********************
- ********************************************************************/
 
  /*@@
    @routine    CCTKi_InitialiseCactus
    @date       Fri Sep 18 14:05:21 1998
    @author     Tom Goodale
-   @desc 
-   
-   @enddesc 
+   @desc
+               Calls all CCTK-internal initialization routines.
+   @enddesc
    @calls      CCTKi_InitialiseSubsystemDefaults
                CCTKi_ProcessEnvironment
                CCTKi_ProcessCommandLine
@@ -79,119 +61,55 @@ static time_t startuptime;
                CCTKi_InitialiseScheduler
                CCTKi_CallStartupFunctions
                CCTKi_PrintBanners
- 
+
    @var        argc
    @vdesc      The number of command line arguments
    @vtype      int *
    @vio        inout
-   @endvar 
+   @endvar
    @var        argv
    @vdesc      The command line arguments
    @vtype      char ***
    @vio        inout
-   @endvar 
-   @var        ConfigData
+   @endvar
+   @var        config
    @vdesc      Flesh configuration data
    @vtype      tFleshConfig *
    @vio        inout
-   @endvar 
+   @endvar
 
    @returntype int
    @returndesc
                0  - success
    @endreturndesc
 @@*/
-int CCTKi_InitialiseCactus (int *argc, char ***argv, tFleshConfig *ConfigData)
+int CCTKi_InitialiseCactus (int *argc, char ***argv, tFleshConfig *config)
 {
   startuptime = time (NULL);
 
   CCTKi_InitialiseSubsystemDefaults ();
 
-  CCTKi_ProcessEnvironment (argc, argv, ConfigData);
+  CCTKi_ProcessEnvironment (argc, argv, config);
 
-  CCTKi_ProcessCommandLine (argc, argv, ConfigData);
+  CCTKi_ProcessCommandLine (argc, argv, config);
 
   CCTKi_CactusBanner ();
 
-  CCTKi_InitialiseDataStructures (ConfigData);
+  CCTKi_InitialiseDataStructures (config);
 
-  CCTKi_ProcessParameterDatabase (ConfigData);
+  CCTKi_ProcessParameterDatabase (config);
 
   CCTKi_BindingsVariablesInitialise ();
 
   CCTKBindings_RegisterThornFunctions ();
 
-  CCTKi_InitialiseScheduler (ConfigData);
+  CCTKi_InitialiseScheduler (config);
 
-  CCTKi_CallStartupFunctions (ConfigData);
+  CCTKi_CallStartupFunctions (config);
 
   CCTKi_PrintBanners ();
 
   return (0);
-}
-
-/********************************************************************
- *********************     Local Routines   *************************
- ********************************************************************/
-
- /*@@
-   @routine    CCTKi_InitialiseScheduler
-   @date       Fri Sep 17 19:34:55 1999
-   @author     Tom Goodale
-   @desc 
-               Initialise all scheduled items
-   @enddesc 
-   @calls      CCTKi_SetParameterSetMask
-               CCTKi_BindingsParameterRecoveryInitialise
-               CCTKi_BindingsScheduleInitialise
-               CCTKi_DoScheduleSortAllGroups
-               CCTK_SchedulePrint 
-
-   @var        ConfigData
-   @vdesc      Flesh configuration data
-   @vtype      tFleshConfig *
-   @vio        unused
-   @endvar 
-
-   @returntype int
-   @returndesc
-               0  - success
-   @endreturndesc
-@@*/
-static int CCTKi_InitialiseScheduler (tFleshConfig *ConfigData)
-{
-  int i, retcode;
-  const CCTK_INT *cctk_show_schedule;
-  extern void CCTKi_SetParameterSetMask (int mask);
-
-
-  /* avoid compiler warning about unused arguments */
-  ConfigData = ConfigData;
-
-  CCTKi_SetParameterSetMask (PARAMETER_RECOVERY_IN);
-
-  if (CCTKi_BindingsParameterRecoveryInitialise () < 0)
-  {
-    CCTK_Warn (0, __LINE__, __FILE__, "Cactus", "Failed to recover parameters");
-  }
-
-  CCTKi_SetParameterSetMask (PARAMETER_RECOVERY_POST);
-
-  CCTKi_BindingsScheduleInitialise ();
-
-  retcode = CCTKi_DoScheduleSortAllGroups ();
-
-  cctk_show_schedule = (const CCTK_INT *)
-                       CCTK_ParameterGet ("cctk_show_schedule", "Cactus", &i);
-
-  if (*cctk_show_schedule)
-  {
-    CCTK_PRINTSEPARATOR
-    CCTK_SchedulePrint (NULL);
-    CCTK_PRINTSEPARATOR
-  }
-
-  return (retcode);
 }
 
 
@@ -199,9 +117,9 @@ static int CCTKi_InitialiseScheduler (tFleshConfig *ConfigData)
    @routine    CCTK_RunTime
    @date       Tue Oct 3 2000
    @author     Gabrielle Allen
-   @desc 
+   @desc
                Seconds since startup
-   @enddesc 
+   @enddesc
 
    @returntype int
    @returndesc
@@ -211,4 +129,68 @@ static int CCTKi_InitialiseScheduler (tFleshConfig *ConfigData)
 int CCTK_RunTime (void)
 {
   return ((int) (time (NULL) - startuptime));
+}
+
+
+/********************************************************************
+ *********************     Local Routines   *************************
+ ********************************************************************/
+ /*@@
+   @routine    CCTKi_InitialiseScheduler
+   @date       Fri Sep 17 19:34:55 1999
+   @author     Tom Goodale
+   @desc
+               Initialise all scheduled items
+   @enddesc
+   @calls      CCTKi_SetParameterSetMask
+               CCTKi_BindingsParameterRecoveryInitialise
+               CCTKi_BindingsScheduleInitialise
+               CCTKi_DoScheduleSortAllGroups
+               CCTK_SchedulePrint
+
+   @var        config
+   @vdesc      Flesh configuration data
+   @vtype      tFleshConfig *
+   @vio        out
+   @endvar
+
+   @returntype int
+   @returndesc
+               return code of @seeroutine CCTKi_DoScheduleSortAllGroups
+   @endreturndesc
+@@*/
+static int CCTKi_InitialiseScheduler (tFleshConfig *config)
+{
+  int retval;
+  const CCTK_INT *cctk_show_schedule;
+  extern void CCTKi_SetParameterSetMask (int mask);
+
+
+  CCTKi_SetParameterSetMask (PARAMETER_RECOVERY_IN);
+
+  config->recovered = CCTKi_BindingsParameterRecoveryInitialise ();
+  if (config->recovered < 0)
+  {
+    CCTK_Warn (0, __LINE__, __FILE__, "Cactus", "Failed to recover parameters");
+  }
+
+  CCTKi_SetParameterSetMask (PARAMETER_RECOVERY_POST);
+
+  CCTKi_BindingsScheduleInitialise ();
+
+  retval = CCTKi_DoScheduleSortAllGroups ();
+
+  cctk_show_schedule = (const CCTK_INT *)
+                       CCTK_ParameterGet ("cctk_show_schedule", "Cactus", NULL);
+
+  if (*cctk_show_schedule)
+  {
+    puts ("-------------------------------------------------------------------"
+          "-------------");
+    CCTK_SchedulePrint (NULL);
+    puts ("-------------------------------------------------------------------"
+          "-------------");
+  }
+
+  return (retval);
 }
