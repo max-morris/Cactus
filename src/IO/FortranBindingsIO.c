e9f75ae86afc5a045c9dff444b4aff84a7752ff8
Gabrielle Allen <gdallen@illinois.edu>
2002-05-16 15:43:05 +0000

    Fortran binding for CCTK_OutputVar
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@2817 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/IO/FortranBindingsIO.c
+++ src/IO/FortranBindingsIO.c
@@ -18,18 +18,104 @@ static const char *rcsid = "$Header$";
 
 CCTK_FILEVERSION(IO_FortranBindingsIO_c)
 
+/********************************************************************
+ ********************* Local Routine Prototypes *********************
+ ********************************************************************/
+
+/********************************************************************
+ ********************* Other Routine Prototypes *********************
+ ********************************************************************/
 
 void CCTK_FCALL CCTK_FNAME (CCTK_OutputGH)
                            (int *istat, const cGH *GH);
+void CCTK_FCALL CCTK_FNAME (CCTK_OutputVar)
+     (int *istat, const cGH *GH, ONE_FORTSTRING_ARG);
 void CCTK_FCALL CCTK_FNAME (CCTKi_RegisterIOMethod)
                            (int *handle, TWO_FORTSTRING_ARG);
 
+/********************************************************************
+ *********************     Local Data   *****************************
+ ********************************************************************/
 
+/********************************************************************
+ *********************     External Routines   **********************
+ ********************************************************************/
+
+ /*@@
+   @routine    CCTK_OutputGH
+   @date       
+   @author     
+   @desc
+   Fortran binding for CCTK_OutputGH
+   @enddesc
+   @var        istat
+   @vdesc      error code
+   @vtype      int
+   @vio        out
+   @endvar
+   @var        GH
+   @vdesc      grid hierachy
+   @vtype      const GH *
+   @vio        in
+   @endvar
+
+   @returntype int
+   @returndesc
+
+   @endreturndesc
+@@*/
 void CCTK_FCALL CCTK_FNAME (CCTK_OutputGH)
                            (int *istat, const cGH *GH)
 {
   *istat = CCTK_OutputGH (GH);
 }
+
+ /*@@
+   @routine    CCTK_OutputVar
+   @date       
+   @author     
+   @desc
+   Fortran binding for CCTK_OutputVar
+   @enddesc
+   @var        istat
+   @vdesc      error code
+   @vtype      int
+   @vio        out
+   @endva
+   @var        GH
+   @vdesc      grid hierachy
+   @vtype      const GH *
+   @vio        in
+   @endvar
+   @var        name
+   @vdesc      Name of variable to outut
+   @vtype      const char *
+   @vio        in
+   @endvar
+   @returntype int
+   @returndesc
+
+   @endreturndesc
+@@*/
+void CCTK_FCALL CCTK_FNAME (CCTK_OutputVar)
+                           (int *istat, const cGH *GH, ONE_FORTSTRING_ARG)
+{
+  ONE_FORTSTRING_CREATE (var);
+  *istat = CCTK_OutputVar (GH, var);
+  free (var);
+}
+
+
+ /*@@
+   @routine    CCTKi_RegisterIOMethod
+   @date       
+   @author     
+   @desc
+
+   @enddesc
+@@*/
+
+/* FIXME: This function is no longer needed, test can remove */
 
 void CCTK_FCALL CCTK_FNAME (CCTKi_RegisterIOMethod)
                            (int *handle, TWO_FORTSTRING_ARG)
@@ -39,3 +125,4 @@ void CCTK_FCALL CCTK_FNAME (CCTKi_RegisterIOMethod)
   free (thorn);
   free (name);
 }
+
