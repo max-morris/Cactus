bf5ebbed39ea3dcbda21a391d9cb869ae8e5a3d2
Thomas Radke <tradke@aei.mpg.de>
2002-06-12 09:40:22 +0000

    Fixed the CVS header for generated files in a new thorn. The colon was missing.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@2905 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- lib/make/new_thorn.pl
+++ lib/make/new_thorn.pl
@@ -3,10 +3,10 @@
 #  @file      new_thorn.pl
 #  @date      Wed Feb  3 16:28:43 1999
 #  @author    Tom Goodale
-#  @desc 
+#  @desc
 #  Script to make a new thorn
 #  @enddesc
-#  @version $Id$ 
+#  @version $Id$
 #@@*/
 
 my $cctk_home = `pwd`;
@@ -26,7 +26,7 @@ while(&TestName(1,$thorn_name)==0)
 if(!$package)
 {
   @arrangements = &GetToolkits($package_dir);
-  
+
   print "The following arrangements are available:\n";
   foreach $package (@arrangements)
   {
@@ -52,7 +52,7 @@ chdir $package_dir;
 if(! -d "$package")
 {
   print "Creating new arrangement $package\n";
-  
+
   mkdir($package, 0755);
 
 }
@@ -77,21 +77,21 @@ mkdir("test", 0755);
 open(OUT, ">interface.ccl") || die "Cannot create interface.ccl";
 
 print OUT "# Interface definition for thorn $thorn_name\n";
-print OUT "# \$Header\$\n";
+print OUT "# \$Header$\n";
 
 close OUT;
 
 open(OUT, ">param.ccl") || die "Cannot create param.ccl";
 
 print OUT "# Parameter definitions for thorn $thorn_name\n";
-print OUT "# \$Header\$\n";
+print OUT "# \$Header$\n";
 
 close OUT;
 
 open(OUT, ">schedule.ccl") || die "Cannot create schedule.ccl";
 
 print OUT "# Schedule definitions for thorn $thorn_name\n";
-print OUT "# \$Header\$\n";
+print OUT "# \$Header$\n";
 
 close OUT;
 
@@ -106,7 +106,7 @@ for ($i = 0; $i < (@author_names); $i++) {
    print OUT "$author_names[$i] <$author_emails[$i]>";
 }
 print OUT "\n";
-print OUT "CVS info   : \$Header\$\n";
+print OUT "CVS info   : \$Header$\n";
 print OUT "--------------------------------------------------------------------------\n";
 print OUT "\n";
 print OUT "Purpose of the thorn:\n";
@@ -119,7 +119,7 @@ chdir("src");
 open(OUT, ">make.code.defn") || die "Cannot create make.code.defn";
 
 print OUT "# Main make.code.defn file for thorn $thorn_name\n";
-print OUT "# \$Header\$\n";
+print OUT "# \$Header$\n";
 print OUT "\n";
 print OUT "# Source files in this directory\n";
 print OUT "SRCS = \n";
@@ -166,16 +166,10 @@ exit;
 #  @routine    prompt
 #  @date       Wed Feb  3 16:37:12 1999
 #  @author     Tom Goodale
-#  @desc 
+#  @desc
 #  Prompts for something, with an optional default.
 #  Based on defprompt in Cactus 3.0 Runtest
-#  @enddesc 
-#  @calls     
-#  @calledby   
-#  @history 
-#
-#  @endhistory 
-#
+#  @enddesc
 #@@*/
 
 sub prompt {
@@ -198,7 +192,7 @@ sub prompt {
 
       $response = <STDIN>;
 
-      if ($response =~ m/^\s*$/ && $default) 
+      if ($response =~ m/^\s*$/ && $default)
       {
         $result = $default;
       }
@@ -219,14 +213,9 @@ sub prompt {
 #  @routine    GetToolkits
 #  @date       Wed Feb  3 16:45:22 1999
 #  @author     Tom Goodale
-#  @desc 
+#  @desc
 #  Gets a list of the current arrangements.
-#  @enddesc 
-#  @calls     
-#  @calledby   
-#  @history 
-#
-#  @endhistory 
+#  @enddesc
 #@@*/
 
 sub GetToolkits
@@ -269,14 +258,9 @@ sub GetToolkits
 #  @routine    TestName
 #  @date       Sat Dec 16 1.48
 #  @author     Gabrielle Allen
-#  @desc 
+#  @desc
 #  Check thorn/arrangement name is valid
-#  @enddesc 
-#  @calls     
-#  @calledby   
-#  @history 
-#
-#  @endhistory 
+#  @enddesc
 #@@*/
 
 sub TestName
