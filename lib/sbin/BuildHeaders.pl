b9c0bd8642f2c80365a623c4e81d231d7b939d1e
Gabrielle Allen <gdallen@illinois.edu>
2002-08-20 13:40:27 +0000

    Removed print debug statements, cannot believe that I left so many in there.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@2977 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- lib/sbin/BuildHeaders.pl
+++ lib/sbin/BuildHeaders.pl
@@ -42,17 +42,12 @@ sub BuildHeaders
   # Check consistency
   foreach $addingthorn (split(" ",$interface_database{"THORNS"}))
   {
-    print "Adding thorn $addingthorn\n";
     foreach $inc_file1 (split(" ",$interface_database{"\U$addingthorn ADD HEADER"}))
     {
-      print "  Adding headers $inc_file1\n";
       foreach $usingthorn (split(" ",$interface_database{"THORNS"}))
       {
-	print "    Using thorn $usingthorn\n";
 	if ($interface_database{"\U$usingthorn USES SOURCE"} =~ $interface_database{"\U$addingthorn ADD HEADER $inc_file1 TO"})
 	{
-	  print "      PROBLEM adding $interface_database{\"\U$addingthorn ADD HEADER $inc_file1 TO\"}\n";
-	  print "      PROBLEM using $interface_database{\"\U$usingthorn USES SOURCE\"}\n";
 	  &CST_error(1,"$inc_file1 was added in $addingthorn as a header include but is being used as $interface_database{\"\U$addingthorn ADD HEADER $inc_file1 TO\"}  in $usingthorn as a source code include",'',__LINE__,__FILE__);
 	}
       }
