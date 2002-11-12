19e372164be7df9a818ef7d12beb45c85d7f25fe
Thomas Radke <tradke@aei.mpg.de>
2002-11-12 11:25:34 +0000

    Switch back to a typedef for CCTK_POINTER. Added 'typedef const void *CCTK_POINTER_TO_CONST;' which will be used for function alias arguments.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@3030 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctk_Types.h
+++ src/include/cctk_Types.h
@@ -21,18 +21,9 @@
 /* Define stuff for C. */
 #ifdef CCODE
 
-/* As long as we don't have 'typedef const void *CCTK_CONST_POINTER;'
-   we have CCTK_POINTER as a #define so that one can add a const qualifier
-   if needed. */
-#if 0
 typedef void *CCTK_POINTER;
-#else
-#define CCTK_POINTER void *
-#endif
+typedef const void *CCTK_POINTER_TO_CONST;
 typedef void (*CCTK_FPOINTER)(void);
-
-/* DEPRECATED IN BETA 12 */
-typedef void (*CCTK_FN_POINTER)(void);
 
 #define CCTK_STRING const char *
 
