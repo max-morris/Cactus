5d20362743896019fa447074a52164615b39239a
Thomas Radke <tradke@aei.mpg.de>
2002-12-20 14:28:15 +0000

    CVS commit on behalf of Jonathan Thornburg:
    add some more explanation for the CCTK_ERROR_INTERP_POINT_X_RANGE
    error code:
    > /* n.b. the "X" here abbreviates "not" -- the point could be out of range
    > /*      along any coordinate axis */
    
    and add a new error code:
    > /* the grid is too small for the selected interpolation molecule */
    > #define CCTK_ERROR_INTERP_GRID_TOO_TINY (-1001)
    
    
    (Both of these are 31 letters long, which is exactly the maximum length
    that the C standard guarantees that compilers/preprocessors must honor.)
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@3051 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctk_Interp.h
+++ src/include/cctk_Interp.h
@@ -145,7 +145,12 @@ int CCTK_InterpLocalUniform(int N_dims,
  */
 
 /* an interpolation point is outside the range of the input arrays */
+/* n.b. the "X" here abbreviates "not" -- the point could be out of range
+/*      along any coordinate axis */
 #define CCTK_ERROR_INTERP_POINT_X_RANGE	(-1000)
+
+/* the grid is too small for the selected interpolation molecule */
+#define CCTK_ERROR_INTERP_GRID_TOO_TINY (-1001)
 
 #ifdef __cplusplus
 }
