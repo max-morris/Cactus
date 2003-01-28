4ae6037912be79204178ccc9dec0a6d6f5c09cb2
Thomas Radke <tradke@aei.mpg.de>
2003-01-28 16:06:41 +0000

    Undo my last commit which didn't seem to be needed.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@3114 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/main/flesh.cc
+++ src/main/flesh.cc
@@ -12,17 +12,9 @@
 #include "cctk_Flesh.h"
 #include "CactusMainFunctions.h"
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
 static const char *rcsid = "$Header$";
 CCTK_FILEVERSION(main_flesh_cc);
 
-#ifdef __cplusplus
-} // extern "C"
-#endif
 
  /*@@
    @routine    main
