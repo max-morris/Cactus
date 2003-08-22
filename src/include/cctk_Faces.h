4aa6733b2193cc60af9fcb300fc5d7692fa14648
David Rideout <dprideout@gmail.com>
2003-08-22 20:46:00 +0000

    works with 2 byte integers
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@3383 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctk_Faces.h
+++ src/include/cctk_Faces.h
@@ -13,11 +13,16 @@
 #define _CCTK_FACES_H_ 1
 
 /* The set of all faces */
+#if CCTK_INTEGER_PRECISION>=4
 #define CCTK_ALL_FACES 1073741823
 /* (This is 2^30-1, so it will set the 'last' 30 bits to 1.)
  * (The 'top' two bits are useless because there is no unsigned int in
  * Fortran, and face bits come in pairs.)
  */
+#else
+#define CCTK_ALL_FACES 16383
+/* 2^14-1 */
+#endif
 
 /* Here will be placed macros which provide a user friendly interface
  * to a general specification for expressing sets of faces of an
