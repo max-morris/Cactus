846bf9aaff7919cc7080e9af32a861a040c07908
Erik Schnetter <schnetter@cct.lsu.edu>
2003-11-19 16:29:28 +0000

    Use fewer empty lines in [CF]Parameters.h.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@3459 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- lib/sbin/CreateParameterBindings.pl
+++ lib/sbin/CreateParameterBindings.pl
@@ -279,8 +279,8 @@ sub CreateParameterBindings
     push(@data, "#ifdef THORN\_IS\_$thorn");
     push(@data, "#include \"${thorn}_CParameters.h\"");
     push(@data, '#endif');
-    push(@data, "\n");  # workaround for perl 5.004_04 to add a trailing newline
   }
+  push(@data, "\n");  # workaround for perl 5.004_04 to add a trailing newline
   $dataout = join ("\n", @data);
   &WriteFile('include/CParameters.h',\$dataout);
 
@@ -290,8 +290,8 @@ sub CreateParameterBindings
     push(@data, "#ifdef THORN\_IS\_$thorn");
     push(@data, "#include \"${thorn}_FParameters.h\"");
     push(@data, '#endif');
-    push(@data, "\n");  # workaround for perl 5.004_04 to add a trailing newline
   }
+  push(@data, "\n");  # workaround for perl 5.004_04 to add a trailing newline
   $dataout = join ("\n", @data);
   &WriteFile('include/FParameters.h',\$dataout);
 
