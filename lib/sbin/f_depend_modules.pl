04bcac2bd3a0d272e2e38c9d805a8faacc8fd3cc
Erik Schnetter <schnetter@cct.lsu.edu>
2004-02-06 00:39:31 +0000

    Put braces onto extra lines to follow the coding conventions.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@3569 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- lib/sbin/f_depend_modules.pl
+++ lib/sbin/f_depend_modules.pl
@@ -26,46 +26,64 @@ print "$dest:";
 my %modules;
 
 my $line = 0;
-while (<STDIN>) {
+while (<STDIN>)
+{
   ++ $line;
-  if (/^\s*#\s*(\d+)/) {
+  if (/^\s*#\s*(\d+)/)
+  {
     # line number directive from C preprocessor
     $line = $1 - 1;
-  } elsif (/^\s*include\s*['"]([^'"]+)['"]/i) {
+  }
+  elsif (/^\s*include\s*['"]([^'"]+)['"]/i)
+  {
     # include statement
     my $name = $1;
     print " \\\n  $srcdir/$name";
-  } elsif (/^\s*module\s+(\w+)/i) {
+  }
+  elsif (/^\s*module\s+(\w+)/i)
+  {
     # begin of a module
     my $name = $1;
     $modules{$name} = 1;
-  } elsif (/^\s*use\s+(\w+)/i) {
+  }
+  elsif (/^\s*use\s+(\w+)/i)
+  {
     # use statement
     my $name = $1;
     my $found = 0;
-    if (! $found) {
+    if (! $found)
+    {
       # reference to a module in this file?
-      if ($modules{$name}) {
+      if ($modules{$name})
+      {
         $found = 1;
       }
     }
-    if (! $found) {
+    if (! $found)
+    {
       # reference to a module in this thorn?
-    loop: foreach my $suffix (@suffixes) {
-        if (-e "$srcdir/$name$suffix") {
+    loop: foreach my $suffix (@suffixes)
+      {
+        if (-e "$srcdir/$name$suffix")
+        {
           $found = 1;
           print " \\\n  $name$suffix.o";
           last loop;
         }
       }
     }
-    if (! $found) {
+    if (! $found)
+    {
       # reference to a module in another thorn?
-    loop: foreach my $dir (@otherdirs) {
+    loop: foreach my $dir (@otherdirs)
+      {
         # note: we could also use the SUBDIRS from the make.code.defn here
-        foreach my $subdir (".", "include") {
-          foreach my $suffix (@suffixes) {
-            if (-e "$dir/$subdir/$name$suffix.o") {
+        foreach my $subdir (".", "include")
+        {
+          foreach my $suffix (@suffixes)
+          {
+            if (-e "$dir/$subdir/$name$suffix.o")
+            {
               $found = 1;
               print " \\\n  $dir/$subdir/$name$suffix.o";
               last loop;
@@ -74,11 +92,15 @@ while (<STDIN>) {
         }
       }
     }
-    if (! $found) {
+    if (! $found)
+    {
       print STDERR "$srcfile:$line: Warning: While tracing module depencencies: Source file for module \"$name\" not found\n";
-      if (@otherdirs) {
+      if (@otherdirs)
+      {
         print STDERR "   Searched in thorn directory and in [" . join(', ', @otherdirs) . "]\n";
-      } else {
+      }
+      else
+      {
         print STDERR "   Searched in thorn directory only.\n";
       }
     }
