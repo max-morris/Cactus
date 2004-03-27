ccb957544599284dbb389f12cc6a0ede00e4ef9c
Tom Goodale <goodale@cct.lsu.edu>
2004-03-27 21:50:22 +0000

    Use correct perl interpreter when running c_file_processor from the CST to generate the parameter structure names.  In the future we should be able to do this without forking off another interpreter, but this is necessary for now.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@3609 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- lib/sbin/CreateParameterBindings.pl
+++ lib/sbin/CreateParameterBindings.pl
@@ -254,7 +254,7 @@ sub CreateParameterBindings
 # Write this one to a temporary file and read it back in
 # Can probably do this better
 
-  open(OUT, "| perl $cctk_home/lib/sbin/c_file_processor.pl $top/config-data > include/CParameterStructNames_temp.h") || die 'Cannot create CParameterStructNames.h by running c_file_processor.pl';
+  open(OUT, "| $ENV{'PERLINTERP'} $cctk_home/lib/sbin/c_file_processor.pl $top/config-data > include/CParameterStructNames_temp.h") || die 'Cannot create CParameterStructNames.h by running c_file_processor.pl';
 
   foreach $structure (sort keys %structures)
   {
