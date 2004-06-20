a08f5d3482dba5dda8531cb32c4b11e2fe56460c
Thomas Radke <tradke@aei.mpg.de>
2004-06-20 12:56:18 +0000

    Use '#if CCTK_HAVE_REAL*' rather than '#ifdef CCTK_REAL*'.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@3775 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/include/cctk_Complex.h
+++ src/include/cctk_Complex.h
@@ -35,15 +35,15 @@ cctk_complex CCTK_Cmplx##Sqrt  (cctk_complex complex_number);
 
 
 /* declare complex functions for all available precisions */
-#ifdef CCTK_REAL4
+#if HAVE_CCTK_REAL4
 DECLARE_CMPLX_FUNCTIONS (CCTK_Cmplx8, CCTK_REAL4, CCTK_COMPLEX8)
 #endif
 
-#ifdef CCTK_REAL8
+#if HAVE_CCTK_REAL8
 DECLARE_CMPLX_FUNCTIONS (CCTK_Cmplx16, CCTK_REAL8, CCTK_COMPLEX16)
 #endif
 
-#ifdef CCTK_REAL16
+#if HAVE_CCTK_REAL16
 DECLARE_CMPLX_FUNCTIONS (CCTK_Cmplx32, CCTK_REAL16, CCTK_COMPLEX32)
 #endif
 
