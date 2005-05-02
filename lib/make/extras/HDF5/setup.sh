d059f75dd3bb6512ba988cae148387a52591f7de
Thomas Radke <tradke@aei.mpg.de>
2005-05-02 08:58:55 +0000

    Add MPI include/lib paths and libs in case a parallel HDF5 installation was found. This closes PR CactusPUGHIO-1920.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@4036 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- lib/make/extras/HDF5/setup.sh
+++ lib/make/extras/HDF5/setup.sh
@@ -163,6 +163,13 @@ if [ $test_zlib -eq 0 ]; then
   HDF5_LIB_DIRS="$HDF5_LIB_DIRS $LIBZ_DIR"
 fi
 
+# Add MPI libraries for parallel HDF5
+if [ $test_phdf5 -eq 0 ]; then
+  HDF5_LIBS="$HDF5_LIBS \${MPI_LIBS}" 
+  HDF5_INC_DIRS="$HDF5_INC_DIRS \${MPI_INC_DIRS}"
+  HDF5_LIB_DIRS="$HDF5_LIB_DIRS \${MPI_LIB_DIRS}"
+fi
+
 # Finally, add the math lib which might not be linked against by default
 if [ $is_windows -eq 0 ]; then
   HDF5_LIBS="$HDF5_LIBS m"
