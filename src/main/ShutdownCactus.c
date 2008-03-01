a5ffc507ee9471865af73410050d00954bce06b1
Erik Schnetter <schnetter@cct.lsu.edu>
2008-03-01 15:25:31 +0000

    Move the output of the schedule timers and the call to MPI_Finalize from CactusDefaultShutdown to CCTKi_ShutdownCactus, which is executed as the last function.  This also means that drivers don't have to do this any more, as it is now always done by the flesh.
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@4467 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- src/main/ShutdownCactus.c
+++ src/main/ShutdownCactus.c
@@ -16,9 +16,39 @@
 #include "cctk_Parameters.h"
 #include "cctk_Schedule.h"
 
+#ifdef CCTK_MPI
+#include "mpi.h"
+#endif
+
 static const char *rcsid = "$Header$";
 
 CCTK_FILEVERSION(main_ShutdownCactus_c);
+
+#ifdef CCTK_MPI
+extern char MPI_Active;
+#endif
+
+#ifdef CCTK_MPI
+#define CACTUS_MPI_ERROR(xf)                                                  \
+          do                                                                  \
+          {                                                                   \
+            int errcode;                                                      \
+                                                                              \
+                                                                              \
+            if((errcode = xf) != MPI_SUCCESS)                                 \
+            {                                                                 \
+              char mpi_error_string[MPI_MAX_ERROR_STRING+1];                  \
+              int resultlen;                                                  \
+                                                                              \
+                                                                              \
+              MPI_Error_string(errcode, mpi_error_string, &resultlen);        \
+              fprintf(stderr, "MPI Call %s returned error code %d (%s)\n",    \
+                              #xf, errcode, mpi_error_string);                \
+              fprintf(stderr, "At line %d of file %s\n",                      \
+                              __LINE__, __FILE__);                            \
+            }                                                                 \
+          } while (0)
+#endif
 
  /*@@
    @routine    CCTKi_ShutdownCactus
@@ -52,6 +82,17 @@ int CCTKi_ShutdownCactus(tFleshConfig *ConfigData)
   {
     CCTK_SchedulePrintTimes (NULL);
   }
+
+  printf("--------------------------------------------------------------------------------\n"); 
+  printf("Done.\n");
+  fflush(stdout);
+ 
+#ifdef CCTK_MPI
+  if(MPI_Active)
+  {
+    CACTUS_MPI_ERROR(MPI_Finalize());
+  }
+#endif
 
   return 0;
 }
