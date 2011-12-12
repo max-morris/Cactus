23fb1eea440e7650c8b6d23ef3bea4bdea5b98a9
Erik Schnetter <schnetter@cct.lsu.edu>
2011-12-12 02:53:50 +0000

    Allow zero timelevels in STORAGE timelevels
    This allows to simply set timelevels=0 to turn off storage without
    having to put an "if (do_something timelevels > 0)" into schedule.ccl
    files. It is also the only way to turn off storage inside of a GROUP
    of SCHEDULE statement based on a condition (other than scheduling the
    item twice, once with STORAGE, once without).
    
    
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@4774 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- lib/sbin/CreateScheduleBindings.pl
+++ lib/sbin/CreateScheduleBindings.pl
@@ -569,10 +569,10 @@ sub ScheduleBlock
   # add check on number of timelevels in case we were using a parameter
   for($i=0; $i < @$tlist; $i++)
   {
-    $buffer .= "    if(!($$tlist[$i] > 0 && $$tlist[$i]  <= CCTK_MaxTimeLevels(\"$$mem_groups[$i]\")))\n";
+    $buffer .= "    if(!($$tlist[$i] >= 0 && $$tlist[$i]  <= CCTK_MaxTimeLevels(\"$$mem_groups[$i]\")))\n";
     $buffer .= "        CCTK_VWarn(0, __LINE__, __FILE__, CCTK_THORNSTRING,\n";
     $buffer .= "                   \"Tried to schedule %d timelevels for group '%s' in schedule.ccl.\\n\"\n";
-    $buffer .= "                   \"Value must be between 1 and %d (inclusive)\",\n";
+    $buffer .= "                   \"Value must be between 0 and %d (inclusive)\",\n";
     $buffer .= "                   $$tlist[$i], \"$$mem_groups[$i]\", CCTK_MaxTimeLevels(\"$$mem_groups[$i]\"));\n";
     $buffer .= "\n";
   }
@@ -703,10 +703,10 @@ sub ScheduleStatement
     # add check on number of timelevels in case we were using a parameter
     for($i=0; $i < @$groups; $i++)
     {
-      $buffer .= "  if(!($$misc[$i] > 0 && $$misc[$i]  <= CCTK_MaxTimeLevels(\"$$groups[$i]\")))\n";
+      $buffer .= "  if(!($$misc[$i] >= 0 && $$misc[$i]  <= CCTK_MaxTimeLevels(\"$$groups[$i]\")))\n";
       $buffer .= "      CCTK_VWarn(0, __LINE__, __FILE__, CCTK_THORNSTRING,\n";
       $buffer .= "                 \"Tried to schedule %d timelevels for group '%s' in schedule.ccl.\\n\"\n";
-      $buffer .= "                 \"Value must be between 1 and %d (inclusive)\",\n";
+      $buffer .= "                 \"Value must be between 0 and %d (inclusive)\",\n";
       $buffer .= "                 $$misc[$i], \"$$groups[$i]\", CCTK_MaxTimeLevels(\"$$groups[$i]\"));\n";
       $buffer .= "\n";
     }
