a52c61078dcb12e7cf92e36eaa4a9099ae6b5d94
Roland Haas <rhaas@tapir.caltech.edu>
2012-06-17 22:20:35 +0000

    remove accidentally commited duplicate capabilites handling code
    git-svn-id: https://svn.cactuscode.org/flesh/trunk@4837 17b73243-c579-4c4c-a9d2-2d5706c11dac

--- lib/sbin/ConfigurationParser.pl
+++ lib/sbin/ConfigurationParser.pl
@@ -158,34 +158,6 @@ sub CreateConfigurationDatabase
     &CST_error(0, $message);
   }
 
-  # Turn optional capabilities into required capabilities, if the
-  # capability is provided. This way we don't have to treat required
-  # and optional requirements differently.
-  my %providedcaps;
-  foreach my $thorn (sort keys %thorns)
-  {
-      if ($cfg{"\U$thorn\E PROVIDES"})
-      {
-          foreach my $providedcap (split (' ', $cfg{"\U$thorn\E PROVIDES"}))
-          {
-              $providedcaps{$providedcap} = 1;
-          }
-      }
-  }
-  foreach my $thorn (sort keys %thorns)
-  {
-      if ($cfg{"\U$thorn\E OPTIONAL"})
-      {
-          foreach my $optionalcap (split (' ', $cfg{"\U$thorn\E OPTIONAL"}))
-          {
-              if ($providedcaps{$optionalcap})
-              {
-                  $cfg{"\U$thorn\E REQUIRES"} .= " $optionalcap";
-              }
-          }
-      }
-  }
-
   return \%cfg;
 }
 
