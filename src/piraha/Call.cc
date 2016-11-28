0c8de4d434437b3a6d3d4feacd8bf0acdf969da3
Roland Haas <rhaas@aei.mpg.de>
2016-11-28 16:19:54 -0600

    Cactus: increae maximum parfile path length to 1000
    POSIX defines MAX_PATH which is at least 1028 so 1000 seems like a good
    lower bound

--- src/piraha/Call.cc
+++ src/piraha/Call.cc
@@ -92,8 +92,12 @@ static std::string mklower(std::string& in) {
 enum ValueType { PIR_STRING,PIR_INT,PIR_REAL,PIR_BOOL,PIR_VOID };
 
 std::string get_parfile() {
-    char path[500];
-    CCTK_ParameterFilename(500, path);
+    // paths could be up to PATH_MAX
+    // http://pubs.opengroup.org/onlinepubs/009695399/basedefs/limits.h.html
+    // yet man realpath(3) states that PATH_MAX may be -1 in which case there
+    // is no upper limit.
+    char path[1000];
+    CCTK_ParameterFilename(sizeof(path), path);
     char *value = strrchr (path, '/');
     if (value == NULL) {
         value = path;
