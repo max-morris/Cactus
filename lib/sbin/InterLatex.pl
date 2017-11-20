90840a7d20b9549801719904430d5e0e73c66a2c
Roland Haas <rhaas@ncsa.illinois.edu>
2017-11-19 19:04:43 -0600

    Cactus: be more specific when skipping interface implementation(s) key

--- lib/sbin/InterLatex.pl
+++ lib/sbin/InterLatex.pl
@@ -190,7 +190,7 @@ sub ReadInterfaceDatabase
 
       my ($arrangement, $thorn) = ($1, $2);
 
-      next if /^implementation/;
+      next if /^implementations$/;
       next if /^thorns$/;
       next if ($interfaceDatabase{$old_key} !~ /\w/);
    
