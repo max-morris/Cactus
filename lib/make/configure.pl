10fe176bdf78e044c48f19233d37c88e6c0e0a41
Roland Haas <rhaas@ncsa.illinois.edu>
2018-01-24 14:30:14 -0600

    Cactus: be more careful about =~ vs ne in perl scripts
    This anchors "no-fortran-compiler" at the end of the F90 string so that
    it only matches filenames and uses "ne" to check for the string "none"

--- lib/make/configure.pl
+++ lib/make/configure.pl
@@ -51,7 +51,7 @@ sub test_fortran_name
   $use_f90 = 0;
 
   if ($compiler_f90 && $compiler_f90 ne "" && $compiler_f90 ne "none" &&
-      $compiler_f90 !~ /no-fortran-compiler/)
+      $compiler_f90 !~ m!/no-fortran-compiler$!)
   {
     ($retcode,$case, $n_underscores) = &compile_fortran_name($compiler_f90,$opts_f90);
     if ($retcode <= 0)
@@ -60,7 +60,7 @@ sub test_fortran_name
     }
   }
   elsif($compiler_f77 && $compiler_f77 ne "" && $compiler_f77 ne "none" &&
-        $compiler_f77 !~ /no-fortran-compiler/)
+        $compiler_f77 !~ m!/no-fortran-compiler$!)
   {
     ($retcode,$case, $n_underscores) = &compile_fortran_name($compiler_f77,$opts_f77);
     if ($retcode <= 0)
@@ -127,14 +127,14 @@ sub test_fortran_common_name
   $use_f77 = 0;
   $use_f90 = 0;
 
-  if ($compiler_f90 && $compiler_f90 ne "" && $compiler_f90 !~ /none/ &&
-      $compiler_f90 !~ /no-fortran-compiler/)
+  if ($compiler_f90 && $compiler_f90 ne "" && $compiler_f90 ne /none/ &&
+      $compiler_f90 !~ m!/no-fortran-compiler$!)
   {
     ($retcode,$case, $n_underscores) = &compile_fortran_common_name($compiler_f90,$opts_f90);
     if ($retcode<=0) {$use_f90 = 1};
   }
-  elsif($compiler_f77 && $compiler_f77 ne "" && $compiler_f77 !~ /none/ &&
-        $compiler_f77 !~ /no-fortran-compiler/)
+  elsif($compiler_f77 && $compiler_f77 ne "" && $compiler_f77 ne /none/ &&
+        $compiler_f77 !~ m!/no-fortran-compiler$!)
   {
     ($retcode,$case, $n_underscores) = &compile_fortran_common_name($compiler_f77,$opts_f77);
     if ($retcode <=0) {$use_f77 = 1};
