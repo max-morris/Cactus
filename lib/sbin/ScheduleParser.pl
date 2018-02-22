b3768f6c874d0dee26eeb9b8abe251058fe14952
Roland Haas <rhaas@ncsa.illinois.edu>
2018-02-22 09:12:33 -0600

    Cactus: fix regex used to check for Cactus timebins

--- lib/sbin/ScheduleParser.pl
+++ lib/sbin/ScheduleParser.pl
@@ -43,7 +43,7 @@ our @schedule_bins = (
     'SHUTDOWN');
 # A regular expression matching all possible schedule bins, including
 # a CCTK prefix and in upper case
-our $schedule_bin_regexp = 'CCTK_(' . join ('|', @schedule_bins) . ')';
+our $schedule_bin_regexp = '\bCCTK_(' . join ('|', @schedule_bins) . ')\b';
 
 # Check that the schedule bin exists
 my %schedule_bins = ();
