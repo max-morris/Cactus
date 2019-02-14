1a72dc40dd35febc7ffeb9f994f4ab1273a8d986
Steven R. Brandt <sbrandt@cct.lsu.edu>
2019-02-14 16:56:57 -0600

    Parallel testing is now supported

--- lib/sbin/RunTest.pl
+++ lib/sbin/RunTest.pl
@@ -103,27 +103,42 @@ while ($choice !~ /^Q/i)
       # Reset/Initialise Test Statistics
       $rundata = &ResetTestStatistics($rundata,$testdata);
 
+      my $testno = 0;
+      my $parallel_size = defined($ENV{CCTK_TEST_SIZE}) ? 0+$ENV{CCTK_TEST_SIZE} : 1;
+      my $parallel_rank = defined($ENV{CCTK_TEST_RANK}) ? 0+$ENV{CCTK_TEST_RANK} : 1;
+
       # Run all parameter files
-      foreach $thorn (split(" ",$testdata->{"RUNNABLETHORNS"}))
+      foreach $thorn (sort split(" ",$testdata->{"RUNNABLETHORNS"}))
       {
-	foreach $test (split(" ",$testdata->{"$thorn RUNNABLE"}))
-	{
-	  print "------------------------------------------------------------------------\n\n";
-	  print "  Test $thorn: $test \n";
-	  print "    \"$testdata->{\"$thorn $test DESC\"}\"\n";
-	  if ($choice !~ /^O/i)
-	  {
-	    $testdata = &RunTest("log",$test,$thorn,$config_data,$testdata);
-	  }
+        foreach $test (sort split(" ",$testdata->{"$thorn RUNNABLE"}))
+        {
+          $testno++;
+          my $rank_to_run = ($testno % $parallel_size) + 1;
+          
+          print "------------------------------------------------------------------------\n\n";
+          #print "test=$test testno=$testno rank_to_run=$rank_to_run parallel_size=$parallel_size parallel_rank=$parallel_rank\n";
 
-	  $rundata = &CompareTestFiles($test,$thorn,\%runconfig,$rundata,$config_data,$testdata);
+          if ($rank_to_run != $parallel_rank) {
+            print "SKIPPING $test SEE RANK $rank_to_run\n";
+            next;
+          }
 
-	  $rundata = &ReportOnTest($test,$thorn,$rundata,$testdata);
-	  if ($choice =~ /^I/i)
-	  {
-	    &ViewResults($test,$thorn,\%runconfig,$rundata,$testdata);
-	  }
-	}
+          print "  Test $thorn: $test \n";
+          print "    \"$testdata->{\"$thorn $test DESC\"}\"\n";
+
+          if ($choice !~ /^O/i)
+          {
+            $testdata = &RunTest("log",$test,$thorn,$config_data,$testdata);
+          }
+
+          $rundata = &CompareTestFiles($test,$thorn,\%runconfig,$rundata,$config_data,$testdata);
+
+          $rundata = &ReportOnTest($test,$thorn,$rundata,$testdata);
+          if ($choice =~ /^I/i)
+          {
+            &ViewResults($test,$thorn,\%runconfig,$rundata,$testdata);
+          }
+        }
       }
 
       # Write results of all tests
@@ -137,39 +152,39 @@ while ($choice !~ /^Q/i)
       ($ntests,@tests) = &ChooseTests($choice,$testdata);
       for ($i=0;$i<$ntests;$i++)
       {
-	$test  = $tests[2*$i];
-	$thorn = $tests[2*$i+1];
-	print "------------------------------------------------------------------------\n\n";
-	print "  Test $thorn: $test\n";
-	print "    \"$testdata->{\"$thorn $test DESC\"}\"\n";
-	$testdata = &RunTest("log",$tests[2*$i],$tests[2*$i+1],$config_data,$testdata);
-	$rundata = &CompareTestFiles($tests[2*$i],$tests[2*$i+1],\%runconfig,$rundata,$config_data,$testdata);
-	$rundata = &ReportOnTest($tests[2*$i],$tests[2*$i+1],$rundata,$testdata);
-	&ViewResults($tests[2*$i],$tests[2*$i+1],\%runconfig,$rundata,$testdata);
+        $test  = $tests[2*$i];
+        $thorn = $tests[2*$i+1];
+        print "------------------------------------------------------------------------\n\n";
+        print "  Test $thorn: $test\n";
+        print "    \"$testdata->{\"$thorn $test DESC\"}\"\n";
+        $testdata = &RunTest("log",$tests[2*$i],$tests[2*$i+1],$config_data,$testdata);
+        $rundata = &CompareTestFiles($tests[2*$i],$tests[2*$i+1],\%runconfig,$rundata,$config_data,$testdata);
+        $rundata = &ReportOnTest($tests[2*$i],$tests[2*$i+1],$rundata,$testdata);
+        &ViewResults($tests[2*$i],$tests[2*$i+1],\%runconfig,$rundata,$testdata);
       }
     }
     elsif ($choice =~ /^[RS]/i)
     {
       if ($thorn && $test)
       {
-	print "------------------------------------------------------------------------\n\n";
-	print "  Test $thorn: $test \n";
-	print "    \"$testdata->{\"$thorn $test DESC\"}\"\n";
-	if ($choice =~ /^S/i)
-	{
-	  $testdata = &RunTest("log stdout",$test,$thorn,$config_data,$testdata);
-	}
-	else
-	{
-	  $testdata = &RunTest("log",$test,$thorn,$config_data,$testdata);
-	}
-	$rundata = &CompareTestFiles($test,$thorn,\%runconfig,$rundata,$config_data,$testdata);
-	$rundata = &ReportOnTest($test,$thorn,,$rundata,$testdata);
-	&ViewResults($test,$thorn,\%runconfig,$rundata,$testdata);
+        print "------------------------------------------------------------------------\n\n";
+        print "  Test $thorn: $test \n";
+        print "    \"$testdata->{\"$thorn $test DESC\"}\"\n";
+        if ($choice =~ /^S/i)
+        {
+          $testdata = &RunTest("log stdout",$test,$thorn,$config_data,$testdata);
+        }
+        else
+        {
+          $testdata = &RunTest("log",$test,$thorn,$config_data,$testdata);
+        }
+        $rundata = &CompareTestFiles($test,$thorn,\%runconfig,$rundata,$config_data,$testdata);
+        $rundata = &ReportOnTest($test,$thorn,,$rundata,$testdata);
+        &ViewResults($test,$thorn,\%runconfig,$rundata,$testdata);
       }
       else
       {
-	print "  No previous test has been run\n";
+        print "  No previous test has been run\n";
       }
     }
     elsif ($choice =~ /^P/i)
@@ -180,7 +195,7 @@ while ($choice !~ /^Q/i)
       }
       else
       {
-	print "  No previous test has been run\n";
+        print "  No previous test has been run\n";
       }
     }
     elsif ($choice =~ /^C/i)
@@ -188,37 +203,37 @@ while ($choice !~ /^Q/i)
       print "  Options for customization\n";
       if ($test)
       {
-	while ($choice !~ /^[AR]/i)
-	{
-	  print "    Change absolute tolerance for this run ($test) [A]\n";
-	  print "    Change relative tolerance for this run ($test) [R]\n";
-	  $choice = &defprompt("  Select choice: ","");
-	  if ($choice =~ /A/i)
-	  {
-	    $runconfig{"$thorn $test ABSTOL"}{".*"} = &defprompt("  New absolute tolerance: ","$runconfig{\"$thorn $test ABSTOL\"}{\".*\"}");
-	  }
-	  elsif ($choice =~ /R/i)
-	  {
-	    $runconfig{"$thorn $test RELTOL"}{".*"} = &defprompt("  New relative tolerance: ","$runconfig{\"$thorn $test RELTOL\"}{\".*\"}");
-	  }
-	}
+        while ($choice !~ /^[AR]/i)
+        {
+          print "    Change absolute tolerance for this run ($test) [A]\n";
+          print "    Change relative tolerance for this run ($test) [R]\n";
+          $choice = &defprompt("  Select choice: ","");
+          if ($choice =~ /A/i)
+          {
+            $runconfig{"$thorn $test ABSTOL"}{".*"} = &defprompt("  New absolute tolerance: ","$runconfig{\"$thorn $test ABSTOL\"}{\".*\"}");
+          }
+          elsif ($choice =~ /R/i)
+          {
+            $runconfig{"$thorn $test RELTOL"}{".*"} = &defprompt("  New relative tolerance: ","$runconfig{\"$thorn $test RELTOL\"}{\".*\"}");
+          }
+        }
       }
       else
       {
-	while ($choice !~ /^[AR]/i)
-	{
-	  print "    Change absolute tolerance from $runconfig{\"ABSTOL\"} for all further runs [A]\n";
-	  print "    Change relative tolerance from $runconfig{\"RELTOL\"} for all further runs [R]\n";
-	  $choice = &defprompt("  Select choice: ","");
-	  if ($choice =~ /^A/i)
-	  {
-	    $runconfig{"ABSTOL"} = &defprompt("  New absolute tolerance: ","$runconfig{\"ABSTOL\"}");
-	  }
-	  elsif ($choice =~ /^R/i)
-	  {
-	    $runconfig{"RELTOL"} = &defprompt("  New relative tolerance: ","$runconfig{\"RELTOL\"}");
-	  }
-	}
+        while ($choice !~ /^[AR]/i)
+        {
+          print "    Change absolute tolerance from $runconfig{\"ABSTOL\"} for all further runs [A]\n";
+          print "    Change relative tolerance from $runconfig{\"RELTOL\"} for all further runs [R]\n";
+          $choice = &defprompt("  Select choice: ","");
+          if ($choice =~ /^A/i)
+          {
+            $runconfig{"ABSTOL"} = &defprompt("  New absolute tolerance: ","$runconfig{\"ABSTOL\"}");
+          }
+          elsif ($choice =~ /^R/i)
+          {
+            $runconfig{"RELTOL"} = &defprompt("  New relative tolerance: ","$runconfig{\"RELTOL\"}");
+          }
+        }
       }
     }
     elsif ($choice =~ /^Q/i)
