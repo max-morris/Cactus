d84288d0bfda42bd4936170e81fc809dfb609cb9
Roland Haas <rhaas@tapir.caltech.edu>
2020-01-02 17:08:03 -0500

    Cactus: change env var expansion to use ${...} instead of $(...)

--- lib/sbin/CSTUtils.pl
+++ lib/sbin/CSTUtils.pl
@@ -692,10 +692,10 @@ sub ParseConfigInfo
       if(! $options{$1})
       {
         $options{$1} = $2;
-        # scan through option value and replace all "$" followed by "(WORD)" by
+        # scan through option value and replace all "$" followed by "{WORD}" by
         # the value of the env variable "WORD". Allow "$" to be escaped by
         # duplicating it.
-        $options{$1} =~ s#\$(\$|\((\w+)\))#$2 ? $ENV{$2} : "\$"#eg;
+        $options{$1} =~ s#\$(\$|\{(\w+)\})#$2 ? $ENV{$2} : "\$"#eg;
       }
       else
       {
