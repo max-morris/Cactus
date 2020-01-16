f6746b29e93feb5816ba43795ae2aaa7cafa06ad
Roland Haas <rhaas@illinois.edu>
2020-01-16 16:48:49 -0600

    Cactus: report unknown function before incorrect number of function args

--- src/piraha/Call.cc
+++ src/piraha/Call.cc
@@ -396,6 +396,8 @@ smart_ptr<Value> meval(smart_ptr<Group> gr,ExpressionEvaluationData *eedata) {
     } else if(pn == "func") {
         std::string fn = gr->group(0)->substring();
         fn = mklower(fn);
+        // the grammer disallows empyt function arguments foo() so there is
+        // always at least group(0) and group(1)
         smart_ptr<Value> val = meval(gr->group(1),eedata);
         // First, functions with at least one, but potentially more than one argument
         if (fn == "max" || fn == "min") {
@@ -440,99 +442,93 @@ smart_ptr<Value> meval(smart_ptr<Group> gr,ExpressionEvaluationData *eedata) {
             }
             return val;
         }
-        // From here on only functions that take exactly one argument: the majority.
-        else if (gr->groupCount() != 2) {
-            std::ostringstream msg;
-            msg << fn << "() needs exactly one argument, but got" << gr->groupCount()-1
-                << "." << std::endl;
-            std::string par = get_parfile();
-            CCTK_Error(gr->line(),par.c_str(),current_thorn.c_str(),msg.str().c_str());
-        }
         else {
+            // From here on only functions that take exactly one argument: the majority.
+            bool known_function = false;
             if(val->type == PIR_REAL || val->type == PIR_INT) {
                 if(fn == "trunc") {
                     val->ddata = trunc(val->realValue());
                     val->type = PIR_REAL;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "floor") {
                     val->ddata = floor(val->realValue());
                     val->type = PIR_REAL;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "ceil") {
                     val->ddata = ceil(val->realValue());
                     val->type = PIR_REAL;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "sqrt") {
                     val->ddata = sqrt(val->realValue());
                     val->type = PIR_REAL;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "cbrt") {
                     val->ddata = cbrt(val->realValue());
                     val->type = PIR_REAL;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "atan") {
                     val->ddata = atan(val->realValue());
                     val->type = PIR_REAL;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "sin") {
                     val->ddata = sin(val->realValue());
                     val->type = PIR_REAL;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "cos") {
                     val->ddata = cos(val->realValue());
                     val->type = PIR_REAL;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "tan") {
                     val->ddata = tan(val->realValue());
                     val->type = PIR_REAL;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "exp") {
                     val->ddata = exp(val->realValue());
                     val->type = PIR_REAL;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "log") {
                     val->ddata = log(val->realValue());
                     val->type = PIR_REAL;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "abs") {
                     val->ddata = fabs(val->realValue());
                     val->type = PIR_REAL;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "acos") {
                     val->ddata = acos(val->realValue());
                     val->type = PIR_REAL;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "asin") {
                     val->ddata = asin(val->realValue());
                     val->type = PIR_REAL;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "bool") {
                     if(val->type == PIR_REAL) {
                         val->idata = std::lrint(val->ddata);
                     }
                     val->type = PIR_BOOL;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "int") {
                     if(val->type == PIR_REAL) {
                         val->idata = std::lrint(val->ddata);
                         val->type = PIR_INT;
                     }
-                    return val;
+                    known_function = true;;
                 } else if(fn == "real") {
                     if(val->type == PIR_INT) {
                         val->ddata = val->idata;
                         val->type = PIR_REAL;
                     }
-                    return val;
+                    known_function = true;;
                 }
             } else if(val->type == PIR_BOOL) {
                 if(fn == "int") {
                     val->type = PIR_INT;
-                    return val;
+                    known_function = true;;
                 } else if(fn == "real") {
                     val->type = PIR_REAL;
                     val->ddata = val->idata;
-                    return val;
+                    known_function = true;;
                 }
             } else if(val->type == PIR_STRING) {
                 if(fn == "int") {
@@ -551,7 +547,7 @@ smart_ptr<Value> meval(smart_ptr<Group> gr,ExpressionEvaluationData *eedata) {
                         std::string par = get_parfile();
                         CCTK_Error(gr->line(),par.c_str(),current_thorn.c_str(),msg.str().c_str());
                     }
-                    return val;
+                    known_function = true;;
                 } else if(fn == "real") {
                     val->type = PIR_REAL;
                     std::istringstream buf(val->sdata);
@@ -568,7 +564,7 @@ smart_ptr<Value> meval(smart_ptr<Group> gr,ExpressionEvaluationData *eedata) {
                         std::string par = get_parfile();
                         CCTK_Error(gr->line(),par.c_str(),current_thorn.c_str(),msg.str().c_str());
                     }
-                    return val;
+                    known_function = true;;
                 } else if(fn == "bool") {
                     val->type = PIR_BOOL;
                     std::string s = mklower(val->sdata);
@@ -582,16 +578,25 @@ smart_ptr<Value> meval(smart_ptr<Group> gr,ExpressionEvaluationData *eedata) {
                         std::string par = get_parfile();
                         CCTK_Error(gr->line(),par.c_str(),current_thorn.c_str(),msg.str().c_str());
                     }
-                    return val;
+                    known_function = true;;
                 }
             }
-            // if we get here, nothing matched
-            {
+            // check if any errors were detected above
+            if(!known_function) {
                 std::ostringstream msg;
                 msg << "Unknown function: " << fn << "(" << val->type << ")" << std::endl;
                 std::string par = get_parfile();
                 CCTK_Error(gr->line(),par.c_str(),current_thorn.c_str(),msg.str().c_str());
             }
+            if(gr->groupCount() != 2) {
+              // All functions that take exactly one argument, but we got a different number
+              std::ostringstream msg;
+              msg << fn << "() needs exactly one argument, but got" << gr->groupCount()-1
+                  << "." << std::endl;
+              std::string par = get_parfile();
+              CCTK_Error(gr->line(),par.c_str(),current_thorn.c_str(),msg.str().c_str());
+            }
+            return val;
         }
     } else if(pn == "name"||pn == "dname") {
         std::string s = gr->substring();
