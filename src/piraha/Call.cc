cd1a6e9771217586ba05de1acd803c89536b102a
Roland Haas <rhaas@illinois.edu>
2020-01-16 16:34:50 -0600

    Cactus: correct reported numbe of arguments in piraha errors

--- src/piraha/Call.cc
+++ src/piraha/Call.cc
@@ -443,7 +443,7 @@ smart_ptr<Value> meval(smart_ptr<Group> gr,ExpressionEvaluationData *eedata) {
         // From here on only functions that take exactly one argument: the majority.
         else if (gr->groupCount() != 2) {
             std::ostringstream msg;
-            msg << fn << "() needs exactly one argument, but got" << gr->groupCount()
+            msg << fn << "() needs exactly one argument, but got" << gr->groupCount()-1
                 << "." << std::endl;
             std::string par = get_parfile();
             CCTK_Error(gr->line(),par.c_str(),current_thorn.c_str(),msg.str().c_str());
