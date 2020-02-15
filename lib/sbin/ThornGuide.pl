max-morris/Cactus

commit 7022d43046cda3cbddf78cad78f2e54269c11f55

Cactus: remove use lib "." in favor of FindBin::Bin

lib/sbin/ThornGuide.pl
@@ -1,7 +1,6 @@
 #!/usr/local/bin/perl
 
 use strict;
-use lib ".";
 use vars qw($h $help $cctk_home $thornlist $directory $outdir $verbose $debug $outfile $tocdepth);
 #$debug = 1;
 
@@ -60,10 +59,10 @@ my $cactus_style_file = "${cctk_home}doc/latex/cactus";
 ##############
 
 # common procedures used to create the thornguide(s)
-require "$sbin_dir/ThornUtils.pm";
+require "ThornUtils.pm";
 
 # for reading of the thornlist routine: %thorns = &ReadThornlist($thornlist)
-require "$sbin_dir/MakeUtils.pl";
+require "MakeUtils.pl";
 
 #####################
 # INITIAL VARIABLES #

lib/sbin/BuildActiveThorns.pl
@@ -9,14 +9,13 @@
 #  @version $Id$
 #@@*/
 
-use lib ".";
 use FindBin;
 use lib "$FindBin::Bin";
 BEGIN {
 my $sbin_dir = $FindBin::Bin;
 }
 
-require "$sbin_dir/MakeUtils.pl";
+require "MakeUtils.pl";
 
 $package_dir = shift(@ARGV);
 

lib/sbin/BuildHeaders.pl
@@ -14,8 +14,10 @@
 #  @endhistory
 #@@*/
 
-use lib ".";
-require "$sbin_dir/CSTUtils.pl";
+
+use FindBin;
+use lib "$FindBin::Bin";
+require "CSTUtils.pl";
 
 sub BuildHeaders
 {

lib/sbin/CST
@@ -11,7 +11,6 @@
 
 use FindBin;
 use FileHandle;
-use lib ".";
 use Carp;
 
 # Global parameter to track the number of errors from the CST
@@ -92,25 +91,25 @@ sub CheckCSTErrors
 
 use lib $sbin_dir;
 
-require "$sbin_dir/parameter_parser.pl";
-require "$sbin_dir/interface_parser.pl";
-require "$sbin_dir/ScheduleParser.pl";
-require "$sbin_dir/ConfigurationParser.pl";
-require "$sbin_dir/ProcessConfiguration.pl";
-require "$sbin_dir/create_c_stuff.pl";
-require "$sbin_dir/create_fortran_stuff.pl";
-require "$sbin_dir/GridFuncStuff.pl";
-require "$sbin_dir/ImpParamConsistency.pl";
-require "$sbin_dir/CSTUtils.pl";
-require "$sbin_dir/MakeUtils.pl";
-require "$sbin_dir/CreateParameterBindings.pl";
-require "$sbin_dir/CreateImplementationBindings.pl";
-require "$sbin_dir/CreateScheduleBindings.pl";
-require "$sbin_dir/CreateFunctionBindings.pl";
-require "$sbin_dir/BuildHeaders.pl";
-require "$sbin_dir/CreateConfigurationBindings.pl";
-require "$sbin_dir/ConfigScriptParser.pl";
-require "$sbin_dir/Logger.pl";
+require "parameter_parser.pl";
+require "interface_parser.pl";
+require "ScheduleParser.pl";
+require "ConfigurationParser.pl";
+require "ProcessConfiguration.pl";
+require "create_c_stuff.pl";
+require "create_fortran_stuff.pl";
+require "GridFuncStuff.pl";
+require "ImpParamConsistency.pl";
+require "CSTUtils.pl";
+require "MakeUtils.pl";
+require "CreateParameterBindings.pl";
+require "CreateImplementationBindings.pl";
+require "CreateScheduleBindings.pl";
+require "CreateFunctionBindings.pl";
+require "BuildHeaders.pl";
+require "CreateConfigurationBindings.pl";
+require "ConfigScriptParser.pl";
+require "Logger.pl";
 
 #######################################################################
 #

lib/sbin/ConfigurationParser.pl
@@ -11,8 +11,6 @@ use strict;
 #  @version  $Header$
 #@@*/
 
-use FindBin;
-use lib ".";
 my $ccl_file;
 
 #/*@@

lib/sbin/CreateConfigurationBindings.pl
@@ -8,8 +8,9 @@
 #  @version   $Header$
 #@@*/
 
-use lib ".";
-require "$sbin_dir/CSTUtils.pl";
+use FindBin;
+use lib "$sbin_dir";
+require "CSTUtils.pl";
 
 #/*@@
 #  @routine    CreateConfigurationBindings

lib/sbin/CreateImplementationBindings.pl
@@ -1,5 +1,3 @@
-use lib ".";
-
 #/*@@
 #  @file      CreateImplementationBindings.pl
 #  @date      Sun Jul  4 17:09:54 1999

lib/sbin/InterLatex.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl -s
 
 #use strict;
-use lib ".";
 use vars qw($cctk_home $debug $verbose $h $help $document_type $outdir $directory $thornlist $thorn);
 
 #/*@@
@@ -56,14 +55,14 @@ use lib $sbin_dir;
 ##############
 
 # common procedures used to create the thornguide(s)
-require "$sbin_dir/ThornUtils.pm";
+require "ThornUtils.pm";
 
 # for use of create_parametere_database
-require "$sbin_dir/interface_parser.pl";
-require "$sbin_dir/CSTUtils.pl";
+require "interface_parser.pl";
+require "CSTUtils.pl";
 
 # for reading of the thornlist routine: %thorns = &ReadThornlist($thornlist)
-require "$sbin_dir/MakeUtils.pl";
+require "MakeUtils.pl";
 
 ####################
 # GLOBAL VARIABLES #

commit 47f4492e7fc02ee4d9fe70ab02970375608f7717

Cactuts: add missing use lib $sbin_dir to ThornGuide.pl
this bug was introduced in 7022d430

lib/sbin/ThornGuide.pl
@@ -46,9 +46,11 @@ if ($help || $h) {
 $cctk_home  .= '/' if (($cctk_home !~ /\/$/) && (defined $cctk_home));
 
 use FindBin;
+my $sbin_dir;
 BEGIN {
 $sbin_dir = $FindBin::Bin;
 }
+use lib $sbin_dir;
 
 # has to be absolute path because the ThornGuide can be build in different
 # directory depths (doc/ThornGuide/build and configs/X/dox/build)