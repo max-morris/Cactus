9fe2e9541bed620af062d7accd67eec5d616f077
Steven R. Brandt <sbrandt@cct.lsu.edu>
2020-02-19 02:43:15 +0000

    Fix issue 2331

--- src/piraha/Call.cc
+++ src/piraha/Call.cc
@@ -340,7 +340,7 @@ std::string string_reparser(std::string s) {
         std::string out = "";
         for(int i=0;i < m->groupCount(); i++) {
             std::string pn = m->group(i)->getPatternName();
-            if(pn == "any" || pn == "name") {
+            if(pn == "any" || pn == "name" || pn == "stringfirstname") {
                 out += m->group(i)->substring();
             } else if(pn == "stringcomment") {
                 ;
