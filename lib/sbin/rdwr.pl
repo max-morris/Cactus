323c28edbd61113d5e0de299353e6efc31260d06
Erik Schnetter <schnetter@gmail.com>
2020-04-01 19:53:12 -0400

    Flesh: Use correct "_p" suffixes when declaring variables

--- lib/sbin/rdwr.pl
+++ lib/sbin/rdwr.pl
@@ -499,7 +499,7 @@ sub create_macros
                 if ($var_group->{vector} ne "0") {
                   $vname .= "[0]";
                 }
-                my $ivar = get_cap($hash, $th, $var);
+                my $ivar = get_cap($hash, $th, $full_var);
                 $$data .= qq(static int cctki_vi_$ivar = -100; if (cctki_vi_$ivar == -100) cctki_vi_$ivar = CCTK_VarIndex("$vname"); $vtype * restrict const $ivar __attribute__((__unused__)) = (($vtype *) CCTKi_VarDataPtrI(cctkGH, $timelevel, cctki_vi_$ivar));; /* group $group_register */\\\n);
               }
             } else {
