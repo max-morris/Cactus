max-morris/Cactus

commit 6d8a9c0b666ec9071433228f914e4a4538db87cd

Cactus: record elapsed time in test log files

lib/sbin/RunTestUtils.pl
@@ -1073,22 +1073,28 @@ sub RunCactus
   printf "\n  Issuing $command\n";
 
   $retcode = 0;
-  open (CMD, "pwd; $command 2>&1 |");
   open (LOG, "> $testname.log");
+
+  my $start_time = &Time::HiRes::gettimeofday();
+  open (CMD, "pwd; $command 2>&1 |");
 
   while (<CMD>)
   {
     print LOG if ($output =~ /log/);
     print STDOUT if ($output =~ /stdout/);
   }
-  close LOG;
   close CMD;
-
   $retcode = $? >> 8 if($retcode==0);
+  my $end_time = &Time::HiRes::gettimeofday();
+
+  my $elapsed = $end_time - $start_time;
+  printf LOG "  Elapsed time: %.1f s\n", $elapsed if ($output =~ /log/);
+  printf LOG "  Elapsed time: %.1f s\n", $elapsed if ($output =~ /stdout/);
+  close LOG;
 
   print STDOUT "\n\n" if ($output =~ /stdout/);
 
-  return $retcode;
+  return $retcode, $elapsed;
 }
 
 
@@ -1611,12 +1617,10 @@ sub RunTest
   $cmd =~ s/\$nprocs/$config_data->{'NPROCS'}/g;
   $cmd =~ s/\$parfile/$parfile/g;
 
-  my $start_time = &Time::HiRes::gettimeofday();
-  $retcode = &RunCactus($output,$test,$cmd);
-  my $end_time = &Time::HiRes::gettimeofday();
+  $retcode, $elapsed = &RunCactus($output,$test,$cmd);
   chdir $config_data->{"CCTK_DIR"};
 
-  $testdata->{"$thorn $test ELAPSEDTIME"} = $end_time - $start_time;
+  $testdata->{"$thorn $test ELAPSEDTIME"} = $elapsed;
 
   # Deal with the error code
   if($retcode != 0)

commit 0c8f274140f3f0b5d1227de134a20dec7088b618

Cactus: fix handling of exit code after measuring time
6d8a9c0b accidentally set all return codes to the empty string

lib/sbin/RunTestUtils.pl
@@ -1072,7 +1072,7 @@ sub CleanDir
 =over 
 
 =item RunCactus($output, $testname, $command)
- Runs Cactus and returns $retcode.
+ Runs Cactus and returns ($retcode, $elapsed_time).
 
 =back
 
@@ -1639,7 +1639,7 @@ sub RunTest
   $cmd =~ s/\$nprocs/$NPROCS/g;
   $cmd =~ s/\$parfile/$parfile/g;
 
-  $retcode, $elapsed = &RunCactus($output,$test,$cmd);
+  ($retcode, $elapsed) = &RunCactus($output,$test,$cmd);
   chdir $config_data->{"CCTK_DIR"};
 
   $testdata->{"$thorn $test ELAPSEDTIME"} = $elapsed;