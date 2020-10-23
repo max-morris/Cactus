max-morris/Cactus

commit 89f50bf0ded69b929d6678dfaa4947a507964403

Cactus: run tests in parallel if requested

lib/sbin/RunTestUtils.pl
@@ -1111,7 +1111,8 @@ sub RunCactus
   printf "\n  Issuing $command\n";
 
   $retcode = 0;
-  open (LOG, "> $testname.log");
+  open (CMD, "pwd; $command 2>&1 |") or die "Could not start '$command': $!";
+  open (LOG, "> $testname.log") or die "Could not open '$testname.log' for writing: $!";
 
   my $start_time = &Time::HiRes::gettimeofday();
   open (CMD, "pwd; $command 2>&1 |");

lib/sbin/RunTest.pl
@@ -28,6 +28,10 @@ use FindBin;
 use lib "$FindBin::Bin";
 require "RunTestUtils.pl";
 
+use Data::Dumper;
+use IO::Select;
+use POSIX ":sys_wait_h";
+
 # Read options from command line
 $prompt = shift;   
 $prompt =~ tr/A-Z/a-z/;
@@ -105,27 +109,134 @@ while ($choice !~ /^Q/i)
       $rundata = &ResetTestStatistics($rundata,$testdata);
 
       # Run all parameter files
+      # run up to nparallel tests in parallel
+      my $nparallel = ($ENV{'CCTK_TESTSUITE_PARALLEL_TESTS'} or 1);
+      my %running_tests;
+      my $wait_for_test = sub {
+        # get data from pipes if there is any available
+        my $reads = IO::Select->new();
+        my %captures;
+        foreach my $pid (keys %running_tests) {
+          my ($thorn,$test,$STDOUT_CAPTURE,$STDERR,$RESULTS_CAPTURE) = @{$running_tests{$pid}};
+          foreach my $cap ($STDOUT_CAPTURE,$STDERR,$RESULTS_CAPTURE) {
+            $captures{$cap->[0]} = \$cap->[1];
+            $reads->add($cap->[0]);
+          }
+        }
+        while (my @can_read = $reads->can_read()) {
+          my $total_read = 0;
+          foreach my $fh (@can_read) {
+            my $read = sysread($fh, my $s, 4096*4);
+            die "Could not read from pipe: $!" if not defined $read;
+            $total_read += $read;
+            ${$captures{$fh}} .= $s;
+          }
+          last if $total_read == 0; # all handles are at EOF
+        }
+        # peel one finished test of the list of running tests
+        my $finished_test = waitpid(-1, WNOHANG);
+        my $retcode = $?;
+        return if $finished_test <= 0; # no child finished
+        my ($thorn,$test,$STDOUT_CAPTURE,$STDERR_CAPTURE,$RESULTS_CAPTURE) = @{$running_tests{$finished_test}};
+        delete $running_tests{$finished_test};
+
+        # read in all remaining data test's stdout, stderr and results
+        my $slurp = sub {
+          my ($FH) = @_;
+          local $/ = undef;
+          my $data = <$FH>;
+          close $FH;
+          return $data;
+        };
+        my $TESTOUT = $STDOUT_CAPTURE->[1] .= &$slurp($STDOUT_CAPTURE->[0]);
+        my $TESTERR = $STDERR_CAPTURE->[1] .= &$slurp($STDERR_CAPTURE->[0]);
+        my $TESTRESULTS = $RESULTS_CAPTURE->[1] .= &$slurp($RESULTS_CAPTURE->[0]);
+        # inject testdata values into global data structure
+        my %testvalues = %{eval $TESTRESULTS};
+        $testdata->{"NFAILED"} += $testvalues{"NFAILED"};
+        $testdata->{"$thorn failed"} .= " ".$testvalues{"$thorn FAILED"};
+        if($retcode != 0) { # subprocess exited with an error
+          $testdata->{"NFAILED"} += 1;
+          $testdata->{"$thorn failed"} .= " ".$test;
+        }
+        foreach my $key (keys %testvalues) {
+          if ($key =~ /^$thorn $test /) {
+            $testdata->{$key} = $testvalues{$key};
+          }
+        }
+
+        # handle and print output
+	print "------------------------------------------------------------------------\n\n";
+	print "  Test $thorn: $test \n";
+	print "    \"$testdata->{\"$thorn $test DESC\"}\"\n";
+        print STDOUT $TESTOUT;
+        print STDERR $TESTERR;
+
+        $rundata = &CompareTestFiles($test,$thorn,\%runconfig,$rundata,$config_data,$testdata,$retcode);
+
+	$rundata = &ReportOnTest($test,$thorn,$rundata,$testdata);
+	if ($choice =~ /^I/i)
+	{
+	  &ViewResults($test,$thorn,\%runconfig,$rundata,$testdata);
+	}
+      };
       foreach $thorn (sort split(" ",$testdata->{"RUNNABLETHORNS"}))
       {
 	foreach $test (split(" ",$testdata->{"$thorn RUNNABLE"}))
 	{
-	  print "------------------------------------------------------------------------\n\n";
-	  print "  Test $thorn: $test \n";
-	  print "    \"$testdata->{\"$thorn $test DESC\"}\"\n";
-          my $retcode = 0;
+          while(scalar keys %running_tests >= $nparallel) { # wait for a task to finish
+            &$wait_for_test();
+          }
+
+	  print "  Starting test $thorn: $test \n" unless $nparallel == 1;
 	  if ($choice !~ /^O/i)
 	  {
-	    $retcode = &RunTest("log",$test,$thorn,$config_data,$testdata,\%runconfig);
+            pipe(my $STDOUT_RD, my $STDOUT_WR) or die "Could not open pipe: $!";
+            pipe(my $STDERR_RD, my $STDERR_WR) or die "Could not open pipe: $!";
+            pipe(my $RESULTS_RD, my $RESULTS_WR) or die "Could not open pipe: $!";
+            my $pid = fork(); # start a sub-process
+            if(not $pid) {
+              # the child process
+              open(STDOUT, '>&', $STDOUT_WR) or die "Could not dup STDOUT: $!";
+              open(STDERR, '>&', $STDERR_WR) or die "Could not dup STDERR $!";;
+              foreach my $FH ($STDOUT_RD, $STDOUT_WR, $STDERR_RD, $STDERR_WR, $RESULTS_RD) {
+                close $FH or die "Failed to close file: $!";
+              }
+
+              # these are globals updated by RunTest so I initialize them to a
+              # known value that I can accumulate over
+              $testdata->{"NFAILED"} = 0;
+              $testdata->{"$thorn failed"} = "";
+
+              my $retcode = &RunTest("log",$test,$thorn,$config_data,$testdata,\%runconfig);
+
+              # get all values that need to be returned to our caller
+              my %retvalues;
+              foreach my $key (keys %$testdata) {
+                if ($key =~ /^$thorn $test /) {
+                  $retvalues{$key} = $testdata->{$key};
+                }
+              }
+              $retvalues{"$thorn FAILED"} = $testdata->{"$thorn FAILED"};
+              $retvalues{"NFAILED"} = $testdata->{"NFAILED"};
+              $Data::Dumper::Terse = 1;
+              print $RESULTS_WR Dumper(\%retvalues) or die "Could not write results: $!";
+              close($RESULTS_WR) or die "Could not write results: $!";
+
+              exit $retcode;
+            } else {
+              # the parent process
+              foreach my $FH ($STDOUT_WR, $STDERR_WR, $RESULTS_WR) {
+                close $FH or die "Failed to close file: $!";
+              }
+              $running_tests{$pid} = [$thorn,$test,[$STDOUT_RD,""],[$STDERR_RD,""],[$RESULTS_RD,""]];
+            }
 	  }
 
-	  $rundata = &CompareTestFiles($test,$thorn,\%runconfig,$rundata,$config_data,$testdata,$retcode);
-
-	  $rundata = &ReportOnTest($test,$thorn,$rundata,$testdata);
-	  if ($choice =~ /^I/i)
-	  {
-	    &ViewResults($test,$thorn,\%runconfig,$rundata,$testdata);
-	  }
 	}
+      }
+      while(scalar keys %running_tests > 0) { # wait for all tasks to finish
+        &$wait_for_test();
       }
 
       # Write results of all tests

commit f4c9e942d18756cca9070296e061aa050c6ab9d2

Cactus: fix starting tests twice introduced in wrong parallel tests code
this was introduced in 89f50bf0 which used an outdated version of the
branch

lib/sbin/RunTestUtils.pl
@@ -1111,11 +1111,10 @@ sub RunCactus
   printf "\n  Issuing $command\n";
 
   $retcode = 0;
-  open (CMD, "pwd; $command 2>&1 |") or die "Could not start '$command': $!";
   open (LOG, "> $testname.log") or die "Could not open '$testname.log' for writing: $!";
 
   my $start_time = &Time::HiRes::gettimeofday();
-  open (CMD, "pwd; $command 2>&1 |");
+  open (CMD, "pwd; $command 2>&1 |") or die "Could not start '$command': $!";
 
   while (<CMD>)
   {